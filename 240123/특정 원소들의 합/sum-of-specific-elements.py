51b09bec567199ceb897f16f900b3e5edab7893d
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-23 13:43:21 +0900

    Title: 특정 원소들의 합, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240123/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,12 @@
+arr = []
+
+for _ in range(4):
+    arr.append(list(map(int,input().split())))
+
+result = 0
+
+for i in range(4):
+    for j in range(i+1):
+        result += arr[i][j]
+
+print(result)
