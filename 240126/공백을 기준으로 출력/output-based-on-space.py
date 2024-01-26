2f5a96d0897a651b4a528b1db59e3cbf5a3527a4
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-26 22:28:41 +0900

    Title: 공백을 기준으로 출력, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240126/공백을 기준으로 출력/output-based-on-space.py
+++ 240126/공백을 기준으로 출력/output-based-on-space.py
@@ -3,9 +3,23 @@ answer = ""
 words = input().split()
 for word in words:
     answer += word
-    
+
 words = input().split()
 for word in words:
     answer += word
 
 print(answer)
+
+'''
+string1 = input()
+string2 = input()
+
+for elem in string1:
+    if elem != " ":
+        print(elem, end="")
+
+for elem in string2:
+    if elem != " ":
+        print(elem, end="")
+        
+'''
