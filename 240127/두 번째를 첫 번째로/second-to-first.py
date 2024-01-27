ac5332fc53a555e947744fa2a2164328f560f6a7
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-27 12:40:49 +0900

    Title: 두 번째를 첫 번째로, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/두 번째를 첫 번째로/second-to-first.py
+++ 240127/두 번째를 첫 번째로/second-to-first.py
@@ -12,3 +12,16 @@ for i in range(len(string)):
         s += string[i]
 
 print(s)
+
+'''
+string = input()
+
+a = string[0]
+b = string[1]
+
+for i in range(len(string)):
+    if string[i] == b:
+        string = string[:i] + a + string[i+1:]
+
+print(string)
+'''
