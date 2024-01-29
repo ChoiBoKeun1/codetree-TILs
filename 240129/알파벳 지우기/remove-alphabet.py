a463efb2939527ecea92efd06495e1d1695fb66b
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-29 11:26:38 +0900

    Title: 알파벳 지우기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/알파벳 지우기/remove-alphabet.py
+++ 240129/알파벳 지우기/remove-alphabet.py
@@ -19,3 +19,21 @@ for s in s2:
         pass
 
 print(int(n1)+int(n2))
+
+'''
+s1 = input()
+s2 = input()
+
+n1 = ""
+n2 = ""
+
+for s in s1:
+    if ord(s) <= ord('9') and ord(s) >= ord('0'):
+        n1 += s
+
+for s in s2:
+    if ord(s) <= ord('9') and ord(s) >= ord('0'):
+        n2 += s
+
+print(int(n1)+int(n2))
+'''
