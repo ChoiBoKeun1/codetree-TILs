0617a2f466ddb43f0b9b89795e6151b9578444d4
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-30 12:53:44 +0900

    Title: 최소공배수 구하기, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240130/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,15 +1,17 @@
 n,m = map(int,input().split())
 
-if n > m: 
-    n,m = m,n
+def gcd(n,m):
+    if n > m: 
+        n,m = m,n
+
+    while m != 0:
+        tmp = n % m
+        n = m
+        m = tmp
+    
+    return n
 
 mul = n * m
+gcd = gcd(n,m)
 
-while m != 0:
-    tmp = n % m
-    n = m
-    m = tmp
-
-# n 은 최대공약수
-
-print(int(mul / n))
+print(int(mul / gcd))
