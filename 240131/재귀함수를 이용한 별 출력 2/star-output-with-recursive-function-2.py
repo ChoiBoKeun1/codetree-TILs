606641bebc027d7ce40050d663cbc1a568eef705
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-31 15:04:21 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 139ms, Memory: 26MB, Status: Passed - CodeTree

--- 240131/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 240131/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -1,22 +1,17 @@
-N = int(input())
+n = int(input())
 
 def func(n):
     if n == 0:
-        return None
-    else:
-        for _ in range(n):
-            print('*',end=' ')
-        print()
-        func(n-1)
+        return
+    
+    for _ in range(n):
+        print('*',end=' ')
+    print()
+        
+    func(n-1)
 
-def reverse_func(n):
-    if n == N+1:
-        return None
-    else:
-        for _ in range(n):
-            print('*',end=' ')
-        print()
-        reverse_func(n+1)
+    for _ in range(n):
+        print('*',end=' ')
+    print()
 
-func(N)
-reverse_func(1)
+func(n)
