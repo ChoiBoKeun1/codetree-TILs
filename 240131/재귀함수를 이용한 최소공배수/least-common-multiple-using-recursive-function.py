260b305b0fa2fee54a9ecb0c9dc04106ae08e6b3
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-01-31 15:51:52 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240131/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,18 +1,25 @@
 n = int(input())
-arr = list(map(int,input().split()))
+arr = [0] + list(map(int,input().split()))
 
+# 최대공약수
 def gcd(a,b):
-    if b == 0:
+    if b == 0: 
         return a
     else: 
         return gcd(b, a % b)
 
+# 최소공배수
 def lcm(a,b):
-    return int((a * b) / gcd(a,b))
+    return (a * b) // gcd(a,b)
 
-a = arr[0]
+# index번째 까지 인덱스의 숫자중에 가장 큰 값
+def get_lcm_all(index):
+    if index == 1:
+        return arr[1]
 
-for i in range(n-1):
-    a = lcm(a,arr[i+1])
+    # 현재 index 숫자와
+    # 1 ~ index-1 까지의 최소공배수를 구한 결과
+    # 두 숫자의 최소공배수 구한다
+    return lcm(get_lcm_all(index-1), arr[index])
 
-print(a)
+print(get_lcm_all(n))
