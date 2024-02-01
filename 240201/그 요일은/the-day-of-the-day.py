4824687ffc85d0ee7437675ae5ab75b1dcbd73c6
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-01 23:49:16 +0900

    Title: 그 요일은, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240201/그 요일은/the-day-of-the-day.py
+++ 240201/그 요일은/the-day-of-the-day.py
@@ -4,17 +4,22 @@ day = input()
 day_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 days_in_month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 
-# m1 다음월 ~ m2 전월까지의 일수 합
+
 days = 0
-for i in range(m1 + 1, m2):
-    days += days_in_month[i]
 
-# m1월 d1일 ~ m1월 마지막일 까지 days
-days += (days_in_month[m1] - d1 + 1)
+if m1 != m2:
+    # m1 다음월 ~ m2 전월까지의 일수 합
+    for i in range(m1 + 1, m2):
+        days += days_in_month[i]
 
-# m2월 일수
-days += d2
+    # m1월 d1일 ~ m1월 마지막일 까지 days
+    days += (days_in_month[m1] - d1 + 1)
 
+    # m2월 일수
+    days += d2
+else:
+    days += (d2 - d1)
+    
 # m1월 d1일 => 월요일
 if day in day_of_week:
     diff = day_of_week.index(day)
