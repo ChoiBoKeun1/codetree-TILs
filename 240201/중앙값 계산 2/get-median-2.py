e9e8373ae824eecbe890c8187528cc371e146417
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-01 13:08:14 +0900

    Title: 중앙값 계산 2, Time: 159ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/중앙값 계산 2/get-median-2.py
+++ 240201/중앙값 계산 2/get-median-2.py
@@ -2,11 +2,12 @@ n = int(input())
 arr = list(map(int,input().split()))
 
 def findMid(arr, idx):
+    tmpArr = arr[:idx]
     if idx == 1:
-        return arr[0]
+        return tmpArr[0]
     else:
-        arr.sort()
-        return arr[int(idx/2)]
+        tmpArr.sort()
+        return tmpArr[int(idx/2)]
         
 
 for i in range(1,len(arr)+1):
