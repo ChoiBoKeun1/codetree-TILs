8d452695455e0d38dee58e8ddf2d9fbf1c94d532
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-01 13:54:12 +0900

    Title: 비오는 날, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/비오는 날/rainy-day.py
+++ 240201/비오는 날/rainy-day.py
@@ -13,3 +13,22 @@ for info in infos:
 answers = sorted(answers)
 answer = ' '.join(answers[0])
 print(answer)
+
+'''
+n = int(input())
+
+class Forecast:
+    def __init__(self, date, day, weather):
+        self.date, self.day, self.weather = date, day, weather
+
+ans = Forecast('9999-99-99','','')
+for _ in range(n):
+    date, day, weather = map(str, input().split())
+
+    f = Forecast(date, day, weather)
+    if weather == 'Rain':
+        if ans.date >= f.date:
+            ans = f
+
+print(ans.date, ans.day, ans.weather)
+'''
