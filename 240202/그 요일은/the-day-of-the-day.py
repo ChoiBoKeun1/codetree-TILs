22f2000468e2dad437767c773def7374ede85c46
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-02 13:57:05 +0900

    Title: 그 요일은, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/그 요일은/the-day-of-the-day.py
+++ 240202/그 요일은/the-day-of-the-day.py
@@ -32,3 +32,47 @@ else:
     if remainder > diff:
         cnt += 1
     print(cnt)
+
+'''
+m1,d1,m2,d2 = map(int,input().split())
+A = input()
+
+# 1월 1일부터 m월 d일까지 일수
+def num_of_days(m,d):
+    days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+    total_days = 0
+
+    for i in range(1,m):
+        total_days += days[i]
+    
+    total_days += d
+
+    return total_days
+
+def num_of_day(s):
+    if s == "Mon":
+        return 0
+    elif s == "Tue": 
+        return 1
+    elif s == "Wed": 
+        return 2
+    elif s == "Thu": 
+        return 3
+    elif s == "Fri": 
+        return 4
+    elif s == "Sat": 
+        return 5
+    return 6
+
+ans = 0
+start_date = num_of_days(m1,d1)
+end_date = num_of_days(m2,d2)
+cur_day = num_of_day("Mon")
+
+for date in range(start_date, end_date + 1):
+    if cur_day == num_of_day(A):
+        ans += 1
+    cur_day = (cur_day + 1 ) % 7
+
+print(ans)
+'''
