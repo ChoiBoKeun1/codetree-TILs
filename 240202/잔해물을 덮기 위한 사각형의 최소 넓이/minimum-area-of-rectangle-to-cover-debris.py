b13dafe23bec1bcadf4e15c9fa6a39075258fbc1
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-02 21:39:18 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 186ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240202/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240202/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -4,35 +4,38 @@ MAX_L = 2000
 rect1 = tuple(map(int,input().split()))
 rect2 = tuple(map(int,input().split()))
 
-
-check = [
-    [0] * (MAX_L+1)
-    for _ in range(MAX_L+1)
-]
-
 x1,y1,x2,y2 = rect1
-x1,y1 = x1+OFFSET, y1+OFFSET
-x2,y2 = x2+OFFSET, y2+OFFSET
+a1,b1,a2,b2 = rect2
 
-for x in range(x1,x2):
-    for y in range(y1,y2):
-        check[x][y] = 1
+if x1 > a1 and x2 < a2 and y1 > b1 and y2 < b2:
+    print(0)
+else:
+    check = [
+        [0] * (MAX_L+1)
+        for _ in range(MAX_L+1)
+    ]
 
-x1,y1,x2,y2 = rect2
-x1,y1 = x1+OFFSET, y1+OFFSET
-x2,y2 = x2+OFFSET, y2+OFFSET
+    x1,y1 = x1+OFFSET, y1+OFFSET
+    x2,y2 = x2+OFFSET, y2+OFFSET
 
-for x in range(x1,x2):
-    for y in range(y1,y2):
-        if check[x][y] == 1:
-            check[x][y] = 2
-        else:
-            check[x][y] = 0
+    for x in range(x1,x2):
+        for y in range(y1,y2):
+            check[x][y] = 1
 
-area = 0
-for x in range(MAX_L+1):
-    for y in range(MAX_L+1):
-        if check[x][y] == 1 or check[x][y] == 2:
-            area += 1
-            
-print(area)
+    a1,b1 = a1+OFFSET, b1+OFFSET
+    a2,b2 = a2+OFFSET, b2+OFFSET
+
+    for x in range(a1,a2):
+        for y in range(b1,b2):
+            if check[x][y] == 1:
+                check[x][y] = 2
+            else:
+                check[x][y] = 0
+
+    area = 0
+    for x in range(MAX_L+1):
+        for y in range(MAX_L+1):
+            if check[x][y] == 1 or check[x][y] == 2:
+                area += 1
+                
+    print(area)
