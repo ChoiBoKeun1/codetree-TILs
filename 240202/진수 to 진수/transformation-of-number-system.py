c4dab97f56c1fc1a1dbed6c8b391f0d38728a5e7
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-02 14:59:49 +0900

    Title: 진수 to 진수, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/진수 to 진수/transformation-of-number-system.py
+++ 240202/진수 to 진수/transformation-of-number-system.py
@@ -29,3 +29,28 @@ decimal = a_to_decimal(a,n)
 answer = decimal_to_b(b,decimal)
 
 print(answer)
+
+'''
+a,b = map(int,input().split())
+n = input()
+
+digits = []
+
+# 10진수로 변환
+num = 0
+for ch in n:
+    num = num * a + (ord(ch) - ord('0'))
+
+# b진수로 변환
+while True:
+    if num < b:
+        digits.append(num)
+        break
+    
+    digits.append(num % b)
+    num //= b
+
+# 진수 배열을 뒤집어 b진수 출력
+for digit in digits[::-1]:
+    print(digit, end='')
+'''
