ea0396c1fa2a5d2a2c2098f8a4cb9a6fc6560a2d
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-02 19:36:07 +0900

    Title: 신기한 타일 뒤집기, Time: 173ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240202/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,41 @@
+n = int(input())
+
+white = 0
+black = 0
+
+cur_dirc = ""
+
+for _ in range(n):
+    num, dirc = map(str, input().split())
+    num = int(num)
+    if cur_dirc == 'L':
+        if dirc == 'L':
+            white += (num-1)
+            cur_dirc = 'L'
+        elif dirc == 'R':
+            if white > num:
+                white -= num
+            else:
+                white = 0
+            black += num
+            cur_dirc = 'R'
+    elif cur_dirc == 'R':
+        if dirc == 'L':
+            if black > num:
+                black -= num
+            else:
+                black = 0
+            white += num
+            cur_dirc = 'L'
+        elif dirc == 'R':
+            black += (num-1)
+            cur_dirc = 'R'
+    else:
+        if dirc == 'L':
+            white += num
+            cur_dirc = 'L'
+        elif dirc == 'R':
+            black += num
+            cur_dirc = 'R'
+
+print(white, black)
