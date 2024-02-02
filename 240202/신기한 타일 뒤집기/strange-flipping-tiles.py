c6fd030888879fe9f47b9135f7b68064530e3e6a
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-02 20:28:37 +0900

    Title: 신기한 타일 뒤집기, Time: 263ms, Memory: 26MB, Status: Passed - CodeTree

--- 240202/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240202/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,47 +1,35 @@
 n = int(input())
 
-white = 0
-black = 0
+arr = [0 for _ in range(100000)]
 
-cur_dirc = ""
+cur_idx = 50000
+
 
 for _ in range(n):
+    white = 0
+    black = 0
     num, dirc = map(str, input().split())
     num = int(num)
-    if cur_dirc == 'L':
-        if dirc == 'L':
-            if black == 1:
-                white += 1
-            white += (num-1)
-            cur_dirc = 'L'
-        elif dirc == 'R':
-            if white > num:
-                white -= num
-            else:
-                white = 0
-            black += num
-            cur_dirc = 'R'
-   
-   
-    elif cur_dirc == 'R':
-        if dirc == 'L':
-            if black > num:
-                black -= num
-            else:
-                black = 0
-            white += num
-            cur_dirc = 'L'
-        elif dirc == 'R':
-            if white == 1:
-                black += 1
-            black += (num-1)
-            cur_dirc = 'R'
-    else:
-        if dirc == 'L':
-            white += num
-            cur_dirc = 'L'
-        elif dirc == 'R':
-            black += num
-            cur_dirc = 'R'
 
+    if dirc == 'L':
+        for i in range(num):
+            if cur_idx < 0:
+                cur_idx = 99999
+            arr[cur_idx] = 1
+            if i != num-1:
+                cur_idx -= 1
+
+    else:
+        for i in range(num):
+            if cur_idx > 99999:
+                cur_idx = 0
+            arr[cur_idx] = 2
+            if i != num-1:
+                cur_idx += 1
+            
+for elem in arr:
+    if elem == 1:
+        white += 1
+    elif elem == 2:
+        black += 1
 print(white, black)
