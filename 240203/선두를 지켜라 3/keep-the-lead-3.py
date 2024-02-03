74d8346f32cd8c4492ac9ba93e3b78b0a904e52d
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-03 22:45:48 +0900

    Title: 선두를 지켜라 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240203/선두를 지켜라 3/keep-the-lead-3.py
+++ 240203/선두를 지켜라 3/keep-the-lead-3.py
@@ -11,7 +11,7 @@ for _ in range(n):
 time_b = 1
 for _ in range(m):
     v,t = map(int,input().split())
-    for i in range(t):
+    for _ in range(t):
         pos_B[time_b] = pos_B[time_b - 1] + v
         time_b += 1
 
@@ -19,7 +19,7 @@ honor = [0] * 1001
 
 # honor list의 idx 시간에 누가 명예의 전당에 있는지 기록한다.
 # honor[1] : A, [2] : B, [3]: A,B 둘다.
-for i in range(1, time_a + 1):
+for i in range(1, time_a):
     if pos_A[i] > pos_B[i]:
         honor[i] = 1
     elif pos_A[i] < pos_B[i]:
