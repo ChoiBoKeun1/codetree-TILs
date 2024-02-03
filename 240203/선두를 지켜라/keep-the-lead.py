83ebf550fd07df05d01628a6159083cdc715c15a
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-03 15:51:28 +0900

    Title: 선두를 지켜라, Time: 175ms, Memory: 40MB, Status: Passed - CodeTree

--- 240203/선두를 지켜라/keep-the-lead.py
+++ 240203/선두를 지켜라/keep-the-lead.py
@@ -2,7 +2,6 @@ MAX_T = 1000000
 
 n,m = map(int,input().split())
 A, B = [0] * (MAX_T+1), [0] * (MAX_T+1)
-isFirst = [False,False]
 
 time_a = 1
 for _ in range(n):
@@ -18,28 +17,15 @@ for _ in range(m):
         B[time_b] = B[time_b - 1] + v
         time_b += 1
 
-answer = 0
-
-i = 1
-while A[i] == B[i]:
-    i += 1
-if A[i] > B[i]:
-    isFirst[0] = True
-    isFirst[1] = False        
-elif A[i] < B[i]:
-    isFirst[0] = False
-    isFirst[1] = True
-
-for i in range(MAX_T):
-    if A[i] != 0 and A[i] == B[i]:
-        # A가 B를 추월
-        if A[i+1] > B[i+1] and A[i-1] <= B[i-1] and isFirst[1]:
+leader, answer = 0, 0
+for i in range(1, time_a):
+    if A[i] > B[i]:
+        if leader == 2:
             answer += 1
-            isFirst[0] = True
-            isFirst[1] = False
-        # B가 A를 추월
-        elif A[i+1] < B[i+1] and A[i-1] >= B[i-1] and isFirst[0]:
+        leader = 1
+    elif A[i] < B[i]:
+        if leader == 1:
             answer += 1
-            isFirst[0] = False
-            isFirst[1] = True
+        leader = 2
+
 print(answer)
