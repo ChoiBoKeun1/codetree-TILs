15a6143477a2ae3a1060bf456ad0c0807525c740
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-03 22:50:16 +0900

    Title: 선두를 지켜라 3, Time: 207ms, Memory: 40MB, Status: Passed - CodeTree

--- 240203/선두를 지켜라 3/keep-the-lead-3.py
+++ 240203/선두를 지켜라 3/keep-the-lead-3.py
@@ -1,7 +1,6 @@
 MAX_T = 1000000
 n,m = map(int,input().split())
 pos_A, pos_B = [0] * (MAX_T+1), [0] * (MAX_T+1)
-honor = [0] * (MAX_T+1)
 
 time_a = 1
 for _ in range(n):
@@ -17,19 +16,22 @@ for _ in range(m):
         pos_B[time_b] = pos_B[time_b - 1] + v
         time_b += 1
 
-# honor list의 idx 시간에 누가 명예의 전당에 있는지 기록한다.
-# honor[1] : A, [2] : B, [3]: A,B 둘다.
-for i in range(1, time_a):
-    if pos_A[i] > pos_B[i]:
-        honor[i] = 1
-    elif pos_A[i] < pos_B[i]:
-        honor[i] = 2
-    else:
-        honor[i] = 3
 
-answer = 0
+# leader == 1 : A, 2 : B, 3: A/B 둘다.
+leader,answer = 0,0
 for i in range(1,time_a):
-    if honor[i] != honor[i+1]:
-        answer += 1
+    if pos_A[i] > pos_B[i]:
+        if leader != 1:
+            answer += 1
+        leader = 1
+    elif pos_A[i] < pos_B[i]:
+        if leader != 2:
+            answer += 1
+        leader = 2
+    else:
+        if leader != 3:
+            answer += 1
+        leader = 3
+
 
 print(answer)
