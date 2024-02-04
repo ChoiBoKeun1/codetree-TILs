084774920c26b27778e3282fa72aff8ecb23f435
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-04 20:11:53 +0900

    Title: 되돌아오기 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240204/되돌아오기 2/come-back-2.py
+++ 240204/되돌아오기 2/come-back-2.py
@@ -7,6 +7,7 @@ dy = [0,1,0,-1]
 cur_dir = 3
 
 ans = 0
+isBack = False
 for elem in s:
     if elem == 'L':
         cur_dir = (cur_dir - 1 + 4) % 4
@@ -18,6 +19,10 @@ for elem in s:
     
     ans += 1
     if x == 0 and y == 0:
+        isBack = True
         break
+
+if not isBack:
+    ans = -1
 
 print(ans)
