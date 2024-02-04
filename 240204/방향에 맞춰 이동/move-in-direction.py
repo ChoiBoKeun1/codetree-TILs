00c8606e11074df37d8da35c010439e49ac4bb14
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-04 16:35:43 +0900

    Title: 방향에 맞춰 이동, Time: 157ms, Memory: 24MB, Status: Passed - CodeTree

--- 240204/방향에 맞춰 이동/move-in-direction.py
+++ 240204/방향에 맞춰 이동/move-in-direction.py
@@ -1,4 +1,5 @@
 n = int(input())
+# 동남서북
 dx,dy = [1,0,-1,0], [0,-1,0,1]
 x,y = 0,0
 
@@ -6,15 +7,15 @@ for _ in range(n):
     cmd, d = map(str,input().split())
     d = int(d)
     if cmd == 'E':
-        x = x + dx[0] * d
-        y = y + dy[0] * d    
+        move_dir = 0    
     elif cmd == 'S':
-        x = x + dx[1] * d
-        y = y + dy[1] * d
+        move_dir = 1
     elif cmd == 'W':
-        x = x + dx[2] * d
-        y = y + dy[2] * d
+        move_dir = 2
     else:
-        x = x + dx[3] * d
-        y = y + dy[3] * d
+        move_dir = 3
+
+    x += dx[move_dir] * d
+    y += dy[move_dir] * d
+
 print(x,y)
