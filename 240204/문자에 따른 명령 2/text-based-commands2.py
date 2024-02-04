7205f42241445659675cd6c12657cca35f9e67a1
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-04 16:41:38 +0900

    Title: 문자에 따른 명령 2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240204/문자에 따른 명령 2/text-based-commands2.py
+++ 240204/문자에 따른 명령 2/text-based-commands2.py
@@ -6,12 +6,13 @@ dx,dy = [1,0,-1,0],[0,-1,0,1]
 
 move_dir = 3
 
-if s[0] == 'L':
-    move_dir = (move_dir -1 + 4) % 4
-else:
-    move_dir = (move_dir + 1) % 4
-
-x += dx[move_dir]
-y += dy[move_dir]
+for elem in s:
+    if elem == 'L':
+        move_dir = (move_dir -1 + 4) % 4
+    elif elem == 'R':
+        move_dir = (move_dir + 1) % 4
+    else:
+        x += dx[move_dir]
+        y += dy[move_dir]
 
 print(x,y)
