211a48e53ad8d141b24aa0ffd8f2a34b6a2caeb6
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-07 14:44:51 +0900

    Title: 일렬로 서있는 소 2, Time: 115ms, Memory: 73MB, Status: Passed - CodeTree

--- 240207/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240207/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,6 +5,6 @@ ans = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if arr[i] <= arr[j] and arr[j] < arr[k]:
+            if arr[i] <= arr[j] and arr[j] <= arr[k]:
                 ans += 1
 print(ans)
