38ee11ccb32df558b24c024b349f068900ebc951
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-07 14:28:54 +0900

    Title: 모이자, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/모이자/gather.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+ans = 10000
+
+# i번째 집으로 모이자
+for i in range(n):
+    sum_val = 0
+    for j in range(n):
+        sum_val += arr[j] * abs(i-j)
+    ans = min(ans,sum_val)
+
+print(ans)
