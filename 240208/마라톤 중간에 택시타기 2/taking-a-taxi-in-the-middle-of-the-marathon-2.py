7c930c00431370a0d7b096ee783706f747086c5a
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-08 13:18:24 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240208/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -9,13 +9,13 @@ for _ in range(n):
     xs.append(x)
     ys.append(y)
 
-man_dist = 0
 # i 번째 체크포인트를 건너뛰자
 for i in range(1, n-1):
     new_xs = xs[:i] + xs[i+1:]
     new_ys = ys[:i] + ys[i+1:]
 
     # 완전 탐색
+    man_dist = 0
     for j in range(n-2):    
         man_dist += (abs(new_xs[j] - new_xs[j+1]) + abs(new_ys[j] - new_ys[j+1]))
 
