9ecd6e27388ea2fa1a9fd5a93590ec35d8f97d04
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-08 19:16:04 +0900

    Title: G or H 3, Time: 394ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/G or H 3/G-or-H-3.py
+++ 240208/G or H 3/G-or-H-3.py
@@ -6,7 +6,7 @@ for _ in range(n):
     arr[int(a)] = b
 
 ans = 0
-for i in range(1, n+1 - k + 1):
+for i in range(1, 10000 - k + 1):
     sum_val = 0
     for j in range(i, i+k+1):
         if arr[j] == 'G':
