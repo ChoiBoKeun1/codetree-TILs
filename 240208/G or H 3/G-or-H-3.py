4b0ebd75105f730e9c43150b2ab11772fbd8946a
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-08 19:18:02 +0900

    Title: G or H 3, Time: 231ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/G or H 3/G-or-H-3.py
+++ 240208/G or H 3/G-or-H-3.py
@@ -1,18 +1,17 @@
+MAX_NUM = 10000
+
 n,k = map(int,input().split())
-arr = [""] * (10001)
+arr = [0] * (MAX_NUM + 1)
 
 for _ in range(n):
     a,b = map(str,input().split())
-    arr[int(a)] = b
+    arr[int(a)] = 1 if b == 'G' else 2
 
 ans = 0
-for i in range(1, 10000 - k + 1):
+for i in range(MAX_NUM - k + 1):
     sum_val = 0
     for j in range(i, i+k+1):
-        if arr[j] == 'G':
-            sum_val += 1
-        elif arr[j] == 'H':
-            sum_val += 2
+        sum_val += arr[j]
     ans = max(ans, sum_val)
 
 print(ans)
