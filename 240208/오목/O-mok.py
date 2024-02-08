ad1e7ba3f8ed6ffbce6e330d7afdfdb3be07d66e
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-08 15:16:10 +0900

    Title: 오목, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240208/오목/O-mok.py
+++ 240208/오목/O-mok.py
@@ -8,26 +8,26 @@ def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 def check_col(i,j, num):
-    if arr[i][j+1] == num and in_range(i,j+1) and \
-       arr[i][j+2] == num and in_range(i,j+2) and \
-       arr[i][j+3] == num and in_range(i,j+3) and \
-       arr[i][j+4] == num and in_range(i,j+4):
+    if in_range(i,j+1) and arr[i][j+1] == num and \
+       in_range(i,j+2) and arr[i][j+2] == num and \
+       in_range(i,j+3) and arr[i][j+3] == num and \
+       in_range(i,j+4) and arr[i][j+4] == num:
         return True
     return False
 
 def check_row(i,j, num):
-    if arr[i+1][j] == num and in_range(i+1,j) and \
-       arr[i+2][j] == num and in_range(i+2,j) and \
-       arr[i+3][j] == num and in_range(i+3,j) and \
-       arr[i+4][j] == num and in_range(i+4,j):
+    if in_range(i+1,j) and arr[i+1][j] == num and \
+       in_range(i+2,j) and arr[i+2][j] == num and \
+       in_range(i+3,j) and arr[i+3][j] == num and \
+       in_range(i+4,j) and arr[i+4][j] == num:
         return True
     return False
 
 def check_diagonal(i,j, num):
-    if arr[i+1][j+1] == num and in_range(i+1,j+1) and \
-       arr[i+2][j+2] == num and in_range(i+2,j+2) and \
-       arr[i+3][j+3] == num and in_range(i+3,j+3) and \
-       arr[i+4][j+4] == num and in_range(i+4,j+4):
+    if in_range(i+1,j+1) and arr[i+1][j+1] == num and \
+       in_range(i+2,j+2) and arr[i+2][j+2] == num and \
+       in_range(i+3,j+3) and arr[i+3][j+3] == num and \
+       in_range(i+4,j+4) and arr[i+4][j+4] == num:
         return True
     return False
 
@@ -36,12 +36,28 @@ ans_i, ans_j = 0,0
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
-            if check_col(i,j,1) or check_row(i,j,1) or check_diagonal(i,j,1):
+            if check_col(i,j,1):
+                ans = 1
+                ans_i, ans_j = i, j+2
+                break
+            elif check_row(i,j,1):
+                ans = 1
+                ans_i, ans_j = i+2, j
+                break
+            elif check_diagonal(i,j,1):
                 ans = 1
                 ans_i, ans_j = i+2, j+2
-                break 
-        if arr[i][j] == 2:
-            if check_col(i,j,2) or check_row(i,j,2) or check_diagonal(i,j,2):
+                break
+        elif arr[i][j] == 2:
+            if check_col(i,j,2):
+                ans = 2
+                ans_i, ans_j = i, j+2
+                break
+            elif check_row(i,j,2):
+                ans = 2
+                ans_i, ans_j = i+2, j
+                break
+            elif check_diagonal(i,j,2):
                 ans = 2
                 ans_i, ans_j = i+2, j+2
                 break 
@@ -51,4 +67,4 @@ for i in range(n):
 
 print(ans)
 if ans:
-    print(ans_j, ans_i)
+    print(ans_i+1, ans_j+1)
