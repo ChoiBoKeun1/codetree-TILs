412f6416ba521b4dc9cf03ebf784e35f9382f2ce
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-08 15:28:01 +0900

    Title: 오목, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240208/오목/O-mok.py
+++ 240208/오목/O-mok.py
@@ -1,3 +1,5 @@
+import sys
+
 n = 19
 arr = [
     list(map(int,input().split()))
@@ -7,80 +9,31 @@ arr = [
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def check_col(i,j, num):
-    if in_range(i,j+1) and arr[i][j+1] == num and \
-       in_range(i,j+2) and arr[i][j+2] == num and \
-       in_range(i,j+3) and arr[i][j+3] == num and \
-       in_range(i,j+4) and arr[i][j+4] == num:
-        return True
-    return False
+# 8방향
+# 남 남동 동 북동 북 북서 서 남서
+dxs = [1,1,0,-1,-1,-1,0,1]
+dys = [0,1,1,1,0,-1,-1,-1]
 
-def check_row(i,j, num):
-    if in_range(i+1,j) and arr[i+1][j] == num and \
-       in_range(i+2,j) and arr[i+2][j] == num and \
-       in_range(i+3,j) and arr[i+3][j] == num and \
-       in_range(i+4,j) and arr[i+4][j] == num:
-        return True
-    return False
-
-def check_diagonal(i,j, num):
-    if in_range(i+1,j+1) and arr[i+1][j+1] == num and \
-       in_range(i+2,j+2) and arr[i+2][j+2] == num and \
-       in_range(i+3,j+3) and arr[i+3][j+3] == num and \
-       in_range(i+4,j+4) and arr[i+4][j+4] == num:
-        return True
-    return False
-
-def check_diagonal_reverse(i,j, num):
-    if in_range(i+1,j-1) and arr[i+1][j-1] == num and \
-       in_range(i+2,j-2) and arr[i+2][j-2] == num and \
-       in_range(i+3,j-3) and arr[i+3][j-3] == num and \
-       in_range(i+4,j-4) and arr[i+4][j-4] == num:
-        return True
-    return False
-
-ans = 0
-ans_i, ans_j = 0,0
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 1:
-            if check_col(i,j,1):
-                ans = 1
-                ans_i, ans_j = i, j+2
-                break
-            elif check_row(i,j,1):
-                ans = 1
-                ans_i, ans_j = i+2, j
-                break
-            elif check_diagonal(i,j,1):
-                ans = 1
-                ans_i, ans_j = i+2, j+2
-                break
-            elif check_diagonal_reverse(i,j,1):
-                ans = 1
-                ans_i, ans_j = i+2, j-2
-                break    
-        elif arr[i][j] == 2:
-            if check_col(i,j,2):
-                ans = 2
-                ans_i, ans_j = i, j+2
-                break
-            elif check_row(i,j,2):
-                ans = 2
-                ans_i, ans_j = i+2, j
-                break
-            elif check_diagonal(i,j,2):
-                ans = 2
-                ans_i, ans_j = i+2, j+2
-                break
-            elif check_diagonal_reverse(i,j,2):
-                ans = 2
-                ans_i, ans_j = i+2, j-2
-                break 
-    else:
-        continue
-    break
+        if arr[i][j] == 0:
+            continue
+        
+        for dx,dy in zip(dxs,dys):
+            cnt = 1
+            cur_x, cur_y = i, j
 
-print(ans)
-if ans:
-    print(ans_i+1, ans_j+1)
+            while True:
+                nx, ny = cur_x + dx, cur_y + dy
+                if not in_range(nx,ny):
+                    break
+                if arr[nx][ny] != arr[i][j]:
+                    break
+                cnt += 1
+                cur_x, cur_y = nx, ny
+            
+            if cnt == 5:
+                print(arr[i][j])
+                print(i + 2*dx + 1, j + 2*dy + 1)
+                sys.exit()
+print(0)
