0fb1e1f7b380695e9c58119eec8a1c7a227d59d0
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-09 16:47:52 +0900

    Title: 밭의 높이를 고르게하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240209/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240209/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -5,12 +5,15 @@ arr = list(map(int,input().split()))
 
 ans = sys.maxsize
 
-for i in range(n):
-    for j in range(i+t-1, n):
-        # 구간 i ~ j 까지
-        cost = 0
-        for k in range(i,j+1):
-            cost += abs(h - arr[k])
-        ans = min(ans,cost)
+# 시작위치 i
+# 0 ~ n-t
+for i in range(n-t + 1):
+    cost = 0
+
+    # 구간 i ~ i+t -1 까지
+    for j in range(i, i+t):
+        cost += abs(h - arr[k])
+    
+    ans = min(ans,cost)
 
 print(ans)
