81b68643c9f64b2f14e27933b3f38ebed8fd0ff7
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-09 15:43:05 +0900

    Title: 아름다운 수열 2, Time: 1289ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240209/아름다운 수열 2/beautiful-sequence-2.py
+++ 240209/아름다운 수열 2/beautiful-sequence-2.py
@@ -4,11 +4,15 @@ n,m = map(int,input().split())
 arr_A = list(map(int,input().split()))
 arr_B = list(map(int,input().split()))
 
-all_permutations = list(permutations(arr_B))
-beautiful_lists = [list(perm) for perm in all_permutations]
-
 ans = 0
-for i in range(n-m+1):
-    if arr_A[i:i+m] in beautiful_lists:
-        ans += 1
-print(ans)
+if n < m:
+    print(ans)
+else:
+    all_permutations = list(permutations(arr_B))
+    beautiful_lists = [list(perm) for perm in all_permutations]
+
+
+    for i in range(n-m+1):
+        if arr_A[i:i+m] in beautiful_lists:
+            ans += 1
+    print(ans)
