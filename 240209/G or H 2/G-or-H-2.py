f053221391be5924e086c632e19dc6c459cacb56
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-09 16:35:00 +0900

    Title: G or H 2, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240209/G or H 2/G-or-H-2.py
+++ 240209/G or H 2/G-or-H-2.py
@@ -10,34 +10,24 @@ for _ in range(n):
 ans = 0
 for i in range(MAX_NUM+1):
     for j in range(i, MAX_NUM+1):
+        # 해당 위치에 사람이 없으면 skip
+        if arr[i] == 0 or arr[j] == 0:
+            continue
+
         num_g, num_h = 0,0
-        
-        # 사진에서 양쪽 끝에 있는 사람 idx
-        left,right = 102,-1
         
         # arr[i] ~ arr[j] 까지
         # G, H  개수를 센다
         for k in range(i,j+1):
             if arr[k] == 'G':
                 num_g += 1
-                left = min(left,k)
-                right = max(right,k)
             elif arr[k] == 'H':
                 num_h += 1
-                left = min(left,k)
-                right = max(right,k)
-        
-        length = right - left
 
-        # 이 구간에 G,H 모두 없으면 다음 구간으로 넘어간다.
-        if num_g == 0 and num_h == 0:
-            continue
-        
-        # G,H의 개수가 같으면 ok.
-        if num_g == num_h:
-            ans = max(ans,length)
-        # G 또는 H 둘 중 하나만 있으면 ok
-        elif num_g == 0 or num_h == 0:
+        # G 또는 H 둘 중 하나만 있거나
+        # G, H 개수가 같다면
+        if num_g == 0 or num_h == 0 or num_g == num_h:
+            length = j-i
             ans = max(ans,length)
 
 print(ans)
