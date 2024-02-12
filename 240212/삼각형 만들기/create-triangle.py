71aebef02eba9e50b9a25c378508eb593f872f22
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-12 17:53:43 +0900

    Title: 삼각형 만들기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240212/삼각형 만들기/create-triangle.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = [
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if i == j or j == k or i == k:
+                continue
+            x1,y1 = arr[i]
+            x2,y2 = arr[j]
+            x3,y3 = arr[k]
+            if x1-x2 != 0 and x1-x3 != 0 and x2-x3 != 0:
+                continue
+            if y1-y2 != 0 and y1-y3 !=0 and y2-y3 != 0:
+                continue
+
+            area = abs((x1*y2 + x2*y3 + x3*y1)-(x2*y1 + x3*x2 + x1*y3))
+            ans = max(area,ans)
+
+print(ans)
