e82d9b45c36e0313871ed9b85e712f68cf58f692
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-12 18:00:28 +0900

    Title: 삼각형 만들기, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240212/삼각형 만들기/create-triangle.py
+++ 240212/삼각형 만들기/create-triangle.py
@@ -10,12 +10,14 @@ for i in range(n):
         for k in range(n):
             if i == j or j == k or i == k:
                 continue
+            
             x1,y1 = arr[i]
             x2,y2 = arr[j]
             x3,y3 = arr[k]
-            if x1-x2 != 0 and x1-x3 != 0 and x2-x3 != 0:
+            
+            if x1 != x2 and x1 != x3 and x2 != x3:
                 continue
-            if y1-y2 != 0 and y1-y3 !=0 and y2-y3 != 0:
+            if y1 != y2 and y1 != y3 and y2 != y3:
                 continue
 
             area = abs((x1*y2 + x2*y3 + x3*y1)-(x2*y1 + x3*x2 + x1*y3))
