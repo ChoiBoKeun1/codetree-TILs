3a7b1d41e5cf75474176dad7c3c7143af508603b
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-13 16:39:09 +0900

    Title: 겹치지 않는 선분 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240213/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240213/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -6,24 +6,20 @@ arr = [
     for _ in range(n)
 ]
 
-check = [0] * n
-
-
+ans = 0
 for i in range(n):
+    overlap = False
+
     for j in range(i+1, n):
         x11,x12 = arr[i]
         x21,x22 = arr[j]
 
         # 선분이 겹치려면..
-        if x11 <= x21 and x12 >= x22:
-            check[i] += 1
-            check[j] += 1
-        elif x11 >= x21 and x12 <= x22:
-            check[i] += 1
-            check[j] += 1
-
-ans = 0
-for elem in check:
-    if elem == 0:
+        if (x11 <= x21 and x12 >= x22) or (x11 >= x21 and x12 <= x22):
+            overlap = True
+            break
+    
+    if overlap == False:
         ans += 1
+
 print(ans)
