b3558d71407394bca689827e97974f13376a4319
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-14 12:56:34 +0900

    Title: 스승의 은혜 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240214/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -4,23 +4,27 @@ arr = [
     for _ in range(n)
 ]
 
-total_price = 0
-cnt = 0
 arr.sort()
-
+ans = 0
 for i in range(n):
-    total_price += arr[i] // 2
-    cnt += 1
-    for j in range(n):
-        if i == j:
-            continue
-        
-        total_price += arr[j]
+    cnt = 0
+    total_price = arr[i] // 2
+    
+    if total_price <= b:
         cnt += 1
-        if total_price > b:
-            break
-    else:
-        continue
-    break
 
-print(cnt)
+        for j in range(n):
+            if i == j:
+                continue
+            
+            total_price += arr[j]
+            
+            if total_price > b:
+                break
+
+            cnt += 1
+        
+        ans = max(ans,cnt)
+    
+
+print(ans)
