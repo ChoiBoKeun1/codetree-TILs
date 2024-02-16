a4b9e6ba28c49edb994e3c911fb84277d2a92aaf
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-16 17:03:35 +0900

    Title: 흥미로운 숫자 2, Time: 220ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240216/흥미로운 숫자 2/interesting-numbers-2.py
@@ -2,18 +2,18 @@ x,y = map(int,input().split())
 
 ans = 0
 for i in range(x,y+1):
-    num = [0] * 10
+    digit = [0] * 10
+    total_digit = len(str(i))
     for s in str(i):
-        num[int(s)] += 1
+        digit[int(s)] += 1
 
-    zero_cnt = 0
-    non_zero_list = []
-    for n in num:
-        if n == 0:
-            zero_cnt += 1
-        else:
-            non_zero_list.append(n)
-    if zero_cnt == 8:
-        if non_zero_list[0] == 1 or non_zero_list[1] == 1:
-            ans += 1
+    interesting = False
+
+    for j in range(10):
+        if digit[j] == total_digit-1:
+            interesting = True
+
+    if interesting:
+        ans += 1
+
 print(ans)
