edd80c9c5cc313f7538585ca464b805debe52c36
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-16 17:19:59 +0900

    Title: 등차수열, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/등차수열/arithmetic-sequence.py
+++ 240216/등차수열/arithmetic-sequence.py
@@ -4,11 +4,13 @@ arr = list(map(int,input().split()))
 # k 찾기
 ans = 0
 for k in range(1,101):
+    cnt = 0
     # arr 에서 숫자 두개를 가져와서
     for i in range(n):
         for j in range(i+1,n):
             if i == j:
                 continue
             if arr[j] - k == k - arr[i]:
-                ans += 1
+                cnt += 1
+    ans = max(ans,cnt)
 print(ans)
