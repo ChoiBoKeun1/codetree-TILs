71ccb8323e51e21c4497b331ebd002b6af2a2506
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-16 23:54:25 +0900

    Title: 빙산의 일각 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240216/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -8,23 +8,18 @@ ans = 0
 # i : 해수면 높이
 for i in range(1,1001):
     cnt = 0
-    isVisit = [False] * n
 
-    for j in range(n):
-        if h[j]-i > 0 and not isVisit[j]:
-            isVisit[j] = True
-            
-            for k in range(j+1, n):
-                if j == k:
-                    continue
-                    
-                if h[k]-i > 0:
-                    isVisit[k] = True
-                else:
-                    cnt += 1
-                    break
-            
-    if isVisit[n-1] == True:
+    # 가장 왼쪽에 빙산이 있으면 추가. 
+    if h[0] > i:
         cnt += 1
+
+    # j-1번째 빙산은 잠기고
+    # j번째 빙산은 떠있으면
+    # j번째부터 시작하는 빙산이 있다.
+    for j in range(1,n):
+        if h[j] > i and h[j-1] <= i:
+            cnt += 1
+    
     ans = max(ans,cnt)
+
 print(ans)
