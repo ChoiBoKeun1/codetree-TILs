8b1c8ba2d46a5127b93842a04d5dede4915bb91a
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-17 12:11:57 +0900

    Title: 야바위, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240217/야바위/ya-rock.py
+++ 240217/야바위/ya-rock.py
@@ -11,7 +11,7 @@ for i in range(1,4):
     stone = [0] * 4
     stone[i] = 1
 
-    for j in range(1,4):
+    for j in range(1,n+1):
         a,b,c = arr[j]
         stone[a],stone[b] = stone[b],stone[a]
         if stone[c]:
