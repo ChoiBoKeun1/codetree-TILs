2f305e89145594be0bfc9c683e77aff5391c9994
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-19 23:42:45 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 177ms, Memory: 25MB, Status: Passed - CodeTree

--- 240219/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240219/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -3,22 +3,34 @@ import sys
 n = int(input())
 string = input()
 
-ans_str_len = sys.maxsize
+check = [False] * (n+1)
 
-# i 부분문자열의 길이
-for i in range(1,n+1):
-    cnt = 0
-    # j 부분문자열 시작위치
-    for j in range(n):
-        if j+i <= n:
-            sub_str = string[j:j+i]
+# j 부분문자열 시작위치
+for j in range(n):
+    # i 부분문자열의 길이
+    for i in range(1,n+1):
+        if not check[i]:
+            cnt = 0
+            if j+i <= n:
+                sub_str = string[j:j+i]
+
+                # string을 순회하며 몇번 나오는지 세보기
+                for k in range(n):
+                    if k+i <= n:
+                        if sub_str == string[k:k+i]:
+                            cnt += 1
             
-            # string을 순회하며 몇번 나오는지 세보기
-            for k in range(n):
-                if k+i <= n:
-                    if sub_str == string[k:k+i]:
-                        cnt += 1
-        if cnt < 2:
-            ans_str_len = min(ans_str_len,i)
-
-print(ans_str_len)
+            # 2번이상 나온경우
+            # 해당하는 문자열 길이는 정답 후보에서 제외한다.
+            if cnt >= 2:
+                check[i] = True
+                continue
+        
+ans = 0
+for i in range(1,n+1):
+    if check[i]:
+        ans += 1
+    else:
+        ans += 1
+        break
+print(ans)
