79ab59fdc9513ad7d350515071f91d4fef82ec47
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-20 22:09:19 +0900

    Title: 가장 작은 x 찾기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240220/가장 작은 x 찾기/find-smallest-x.py
+++ 240220/가장 작은 x 찾기/find-smallest-x.py
@@ -1,21 +1,19 @@
+MAX_NUM = 10000
+
 n = int(input())
-a_list, b_list = [0],[0]
+conditions = [
+    tuple(map(int,input().split()))
+    for _ in range(n)
+]
 
-for _ in range(n):
-    a,b = map(int,input().split())
-    a_list.append(a)
-    b_list.append(b)
+def satisfy(x):
+    for a,b in conditions:
+        x *= 2
+        if x < a or x > b:
+            return False
+    return True
 
-ans = 10001
-for i in range(1,10001):
-    isPossible = True
-    for j in range(1,n+1):
-        val = i * (2**j)
-        if a_list[j] <= val and val <= b_list[j]:
-            continue
-        else:
-            isPossible = False
-            break   
-    if isPossible:
-        ans = min(ans,i)
-print(ans)
+for x in range(1,MAX_NUM+1):
+    if satisfy(x):
+        print(x)
+        break
