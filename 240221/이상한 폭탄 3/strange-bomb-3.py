ce71460fe38cde784633210ca7dcf524448a83e3
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-21 18:54:50 +0900

    Title: 이상한 폭탄 3, Time: 124ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240221/이상한 폭탄 3/strange-bomb-3.py
+++ 240221/이상한 폭탄 3/strange-bomb-3.py
@@ -1,38 +1,35 @@
-n,k = map(int,input().split())
-arr = [int(input()) for _ in range(n)]
-
 MAX_NUM = 1000000
-boom = [False] * (n)
-boom_cnt = [0] * (MAX_NUM+1)
 
-maxval = 1
+n,k = map(int,input().split())
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+bomb = [0] * (MAX_NUM+1)
+explode = [False] * n
+
+
+maxval = 0
 maxidx = 0
-
 for i in range(n):
-    for j in range(i+1, n):
+    for j in range(i+1,n):
         if j-i > k:
             break
         
         if arr[i] != arr[j]:
             continue
 
-        if not boom[i]:
-            boom[i] = True
-            boom[j] = True
-            boom_cnt[arr[i]] += 1
-            boom_cnt[arr[j]] += 1
-        else:
-            boom[j] = True
-            boom_cnt[arr[j]] += 1
+        if not explode[i]:
+            explode[i] = True
+            bomb[arr[i]] += 1
+        
+        if not explode[j]:
+            explode[j] = True
+            bomb[arr[j]] += 1
 
-blown_bomb = 0
-ans = 0
-for i in range(n):
-    if boom[i]:
-        if blown_bomb < boom_cnt[i]:
-            blown_bomb = boom_cnt[i]
-            ans = i
-        if blown_bomb == boom_cnt[i]:
-            if ans < i:
-                ans = i
-print(ans)
+for i in range(MAX_NUM+1):
+    if maxval <= bomb[i]:
+        maxval = bomb[i]
+        maxidx = i
+print(maxidx)
