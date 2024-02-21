626ef5a69d79fd2197a6ba96e3eab2523f6ac549
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-21 22:40:52 +0900

    Title: 구간 잘 나누기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/구간 잘 나누기/divide-sections-well.py
+++ 240221/구간 잘 나누기/divide-sections-well.py
@@ -1,27 +1,37 @@
-import sys
+MAX_ANS = 10000
 
 n,m = map(int,input().split())
 arr = list(map(int,input().split()))
 
-def calculate_max_sum(barriers):
-    partitions = []
+ans = MAX_ANS
+for i in range(1, MAX_ANS+1):
+    # 구간의 합의 최댓값이 i이다.
     
-    # 초기 칸막이 위치 설정
-    barriers = [0] + [barriers] + [len(arr) - 1]
-    
-    # 칸막이 위치에 따라 구간을 나누어 구간 합 계산
-    for i in range(1, len(barriers) - m + 2):
-        partition = arr[barriers[i - 1]:barriers[i]]
-        partitions.append(sum(partition))
-    
-    return max(partitions)
+    # possible 구간을 나눌수 있으면 true
+    # section 구간 개수
+    possible = True
+    section = 1
 
-ans = sys.maxsize
+    # 구간 합
+    section_sum = 0
+    for j in range(n):
+        # 어떤 숫자가 '구간 합 최댓값'인 i보다 크다 : 모순
+        # 즉시 break. 이번 i는 최댓값이 될 수 없다는 뜻.
+        if arr[j] > i:
+            possible = False
+            break
+        
+        # j번째 숫자가 들어갔을 때, i 보다 커지면
+        # j번째 숫자부터 다음 구간으로 만든다
+        if section_sum + arr[j] > i:
+            section_sum = 0
+            section += 1
 
-for barriers in range(1,n):
-    if barriers >= m-1:
-        max_part_sum = calculate_max_sum(barriers)
-        ans = min(ans, max_part_sum)
+        # 이 구간에 j번째 숫자를 집어넣는다.
+        section_sum += arr[j]
 
+    # 구간을 나눌 수 있고, 구간의 개수가 m 보다 작거나 같다
+    if possible and section <= m:
+        ans = min(ans,i)
 
 print(ans)
