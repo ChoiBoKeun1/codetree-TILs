d3ecfd7dcfd37b4a562e6f454e89a0f6ca7c6353
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-21 15:13:10 +0900

    Title: 초기 수열 복원하기, Time: 1980ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240221/초기 수열 복원하기/restore-initial-sequence.py
+++ 240221/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,12 +1,27 @@
-from itertools import permutations
-
 n = int(input())
 arr = list(map(int,input().split()))
 
+def generate_permutations(arr):
+    n = len(arr)
+
+    if n == 1:
+        return [(arr[0],)]
+
+    all_permutations = []
+
+    for i in range(n):
+        fixed_element = arr[i]
+        rest = arr[:i] + arr[i+1:]
+
+        rest_permutations = generate_permutations(rest)
+
+        for perm in rest_permutations:
+            all_permutations.append((fixed_element,) + perm)
+
+    return all_permutations
 
 init_arr = [i for i in range(1,n+1)]
-all_permutations = permutations(init_arr)
-possible_arrs = list(all_permutations)
+possible_arrs = generate_permutations(init_arr)
 
 for possible_arr in possible_arrs:
     new_arr = []
