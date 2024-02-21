e7c90c9b45b1215ca605240926b98a2ddd0ace63
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-21 15:29:11 +0900

    Title: 초기 수열 복원하기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240221/초기 수열 복원하기/restore-initial-sequence.py
+++ 240221/초기 수열 복원하기/restore-initial-sequence.py
@@ -1,34 +1,22 @@
 n = int(input())
 arr = list(map(int,input().split()))
 
-def generate_permutations(arr):
-    n = len(arr)
+sequence = [0]*n
 
-    if n == 1:
-        return [(arr[0],)]
-
-    all_permutations = []
-
-    for i in range(n):
-        fixed_element = arr[i]
-        rest = arr[:i] + arr[i+1:]
-
-        rest_permutations = generate_permutations(rest)
-
-        for perm in rest_permutations:
-            all_permutations.append((fixed_element,) + perm)
-
-    return all_permutations
-
-init_arr = [i for i in range(1,n+1)]
-possible_arrs = generate_permutations(init_arr)
-
-for possible_arr in possible_arrs:
-    new_arr = []
-    for i in range(n-1):
-        new_arr.append(possible_arr[i] + possible_arr[i+1])
-
-    if arr == new_arr:
-        for elem in possible_arr:
-            print(elem, end=' ')
-        break
+# 첫번째 원소를 i라 가정
+for i in range(1,n+1):
+    sequence[0] = i
+    
+    for j in range(n-1):
+        sequence[j+1] = arr[j] - sequence[j]
+    
+    # sequence의 모든 원소가 유일한가
+    if len(sequence) == len(set(sequence)):
+        new_sum_arr = []
+        for j in range(n-1):
+            new_sum_arr.append(sequence[j]+sequence[j+1])
+        
+        if arr == new_sum_arr:
+            for elem in sequence:
+                print(elem, end=' ')
+            break
