55103dcf947f5f4d92e8d9a51fb00bb6647f5bec
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-21 14:48:25 +0900

    Title: A, B, C, D 찾기 2, Time: 235ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 240221/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -1,41 +1,14 @@
 arr = list(map(int,input().split()))
 
-ans = []
-
-def func2(tmp_arr, i):
-    if i in tmp_arr:
-        tmp_arr.remove(i)
-    return tmp_arr
-
-def func(arr):
-    for a in range(1,40+1):
-        for b in range(1,40+1):
-            for c in range(1,40+1):
-                for d in range(1,40+1):
-                    tmp_arr = arr[:]
-                    tmp_arr = func2(tmp_arr,d)
-                    tmp_arr = func2(tmp_arr,c)
-                    tmp_arr = func2(tmp_arr,b)
-                    tmp_arr = func2(tmp_arr,a)
-                    tmp_arr = func2(tmp_arr,a+b)
-                    tmp_arr = func2(tmp_arr,b+c)
-                    tmp_arr = func2(tmp_arr,c+d)
-                    tmp_arr = func2(tmp_arr,d+a)
-                    tmp_arr = func2(tmp_arr,a+c)
-                    tmp_arr = func2(tmp_arr,b+d)
-                    tmp_arr = func2(tmp_arr,a+b+c)
-                    tmp_arr = func2(tmp_arr,a+b+d)
-                    tmp_arr = func2(tmp_arr,a+c+d)
-                    tmp_arr = func2(tmp_arr,b+c+d)
-                    tmp_arr = func2(tmp_arr,a+b+c+d)
-
-                    if len(tmp_arr) == 0:
-                        ans.append(a)
-                        ans.append(b)
-                        ans.append(c)
-                        ans.append(d)
-                        return
-
-func(arr)
-for elem in ans:
-    print(elem,end=' ')
+for a in range(1,40+1):
+    for b in range(a,40+1):
+        for c in range(b,40+1):
+            for d in range(c,40+1):
+                arr2 = [
+                    a,b,c,d,
+                    a+b, b+c, c+d, d+a, a+c, b+d,
+                    a+b+c, a+b+d, a+c+d, b+c+d,
+                    a+b+c+d
+                ]
+                if sorted(arr) == sorted(arr2):
+                    print(a,b,c,d)
