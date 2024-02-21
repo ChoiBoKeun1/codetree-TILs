ab25ed8d25a0f47ba3170c3f0b697cb18af7d351
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-21 14:53:22 +0900

    Title: 독서실의 거리두기 4, Time: 270ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240221/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,21 @@
+n = int(input())
+seat = list(input())
+
+def min_dist():
+    dist = n
+    for i in range(n):
+        for j in range(i+1,n):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist,abs(j-i))
+    return dist
+
+ans = 0
+for i in range(n):
+    for j in range(i+1,n):
+        if i == j:
+            continue
+        if seat[i] == '0' and seat[j] == '0':
+            seat[i],seat[j] = '1','1'
+            ans = max(ans, min_dist())
+            seat[i],seat[j] = '0','0'
+print(ans)
