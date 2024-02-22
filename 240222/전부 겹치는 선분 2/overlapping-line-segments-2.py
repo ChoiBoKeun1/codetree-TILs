ea493a1af7f81c80e3a564c7f5391dbfffea0632
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-22 14:15:39 +0900

    Title: 전부 겹치는 선분 2, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240222/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240222/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -4,11 +4,11 @@ arr = [
     for _ in range(n)
 ]
 
-possible = True
 # i번째 선분을 제거
 for i in range(n):
     max_x1 = 0
     min_x2 = 101
+    possible = True
 
     for j in range(n):
         if i == j:
@@ -18,10 +18,12 @@ for i in range(n):
         max_x1 = max(max_x1,x1)
         min_x2 = min(min_x2,x2)
             
-    if max_x1 >= min_x2:
+    if min_x2 < max_x1:
         possible = False    
 
-if possible:
-    print('Yes')
-else:
+    if possible:
+        print('Yes')
+        break
+
+if not possible:
     print('No')
