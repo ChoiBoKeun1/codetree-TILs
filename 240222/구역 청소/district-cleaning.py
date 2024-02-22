e5ff40e5e3b3dac5e6a950c7715cbad539002ce1
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-22 13:31:31 +0900

    Title: 구역 청소, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240222/구역 청소/district-cleaning.py
+++ 240222/구역 청소/district-cleaning.py
@@ -1,12 +1,23 @@
 a,b = map(int,input().split())
 c,d = map(int,input().split())
 
+ans = 0
+# ab가 cd 보다 앞에 있음
+if a <= c:
+    # ab와 cd 일부분이 겹치는 경우
+    if b >= c:
+        ans += (b-a) + (d-c) - (b-c)
+    # ab가 cd를 포함하는 경우
+    elif b >= d:
+        ans += b-a
 
-ans = (b-a) + (d-c)
-
-if b < c or d < a:
-    pass
+# cd가 ab 보다 앞에 있음
 else:
-    ans -= min(abs(b-c),abs(d-a))
+    # ab와 cd 일부분이 겹치는 경우
+    if d >= a:
+        ans += (b-a) + (d-c) - (d-a)
+    # cd가 ab를 포함하는 경우
+    elif d >= b:
+        ans += d-c
 
 print(ans)
