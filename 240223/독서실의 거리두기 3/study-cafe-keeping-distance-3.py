81b4b87d6ed4f7b944b65a4f23d6b677d38115fb
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-23 19:10:07 +0900

    Title: 독서실의 거리두기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,25 @@
+n = int(input())
+seat = list(input())
+
+# 현 상태에서, 가장 가까운 두사람들 중에서, 가장 멀리 떨어져있는 경우를 찾는다
+p1,p2 = -1,-1
+dist = 0
+for i in range(n):
+    for j in range(i+1, n):
+        if seat[i] == '1' and seat[j] == '1':
+            if j-i > dist:
+                dist = j-i
+                p1,p2 = i,j
+            break
+
+# 두 자리 p1,p2 사이 가운데에 새로운 1을 넣는다
+seat[p1+p2 // 2] = '1'
+
+# 새로 만들어진 seat에서, 가장 가까운 두사람들 중에서, 가장 멀리 떨어져있는 경우를 찾는다
+for i in range(n):
+    for j in range(i+1, n):
+        if seat[i] == '1' and seat[j] == '1':
+            if j-i < dist:
+                dist = j-i
+            break
+print(dist)
