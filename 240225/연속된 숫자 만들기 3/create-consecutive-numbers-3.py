0e38a8243957585ebe7bb99faeac069aa2ee0f7f
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-25 22:16:47 +0900

    Title: 연속된 숫자 만들기 3, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240225/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -6,14 +6,13 @@ ans = 0
 if arr[0]+1 == arr[1] and arr[1]+1 == arr[2]:
         ans = 0
 else:
-    left_dist = arr[1]-arr[0]
-    right_dist = arr[2]-arr[1]
+    left_dist = arr[1] - arr[0]
+    right_dist = arr[2] - arr[1]
 
-    ans = abs(right_dist - left_dist)
-
+    ans = max(left_dist, right_dist)-1
 
 '''
-1 2 10
+1 2 10. left_dist = 1, right_dist = 8
 2 9 10  1
 2 3 9   2
 3 8 9   3
@@ -22,6 +21,13 @@ else:
 4 5 7   6
 5 6 7   7
 
+4 7 14. left_dist = 3, right_dist = 7
+7 13 14  1
+7 8 13   2
+8 12 13  3
+8 9 12   4
+9 11 12  5
+9 10 12  6
 '''
 
 print(ans)
