969d5f5f6be95e15764daf78a162d5870aa757f1
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-25 22:12:20 +0900

    Title: 연속된 숫자 만들기 3, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240225/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240225/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,23 +1,27 @@
 arr = list(map(int,input().split()))
 arr.sort()
 
-def isAnswer():
-    if arr[0]+1 == arr[1] and arr[1]+1 == arr[2]:
-        return True
-    return False
-
-
 # 처음부터 연속된 경우
 ans = 0
-while not isAnswer():
+if arr[0]+1 == arr[1] and arr[1]+1 == arr[2]:
+        ans = 0
+else:
     left_dist = arr[1]-arr[0]
     right_dist = arr[2]-arr[1]
 
-    if left_dist <= right_dist:
-        arr[0] = arr[2]-1
-    else:
-        arr[2] = arr[0]+1
-    ans += 1
-    arr.sort()
-    
+    ans = abs(right_dist - left_dist)
+
+
+'''
+1 2 10
+2 9 10  1
+2 3 9   2
+3 8 9   3
+3 4 8   4
+4 7 8   5
+4 5 7   6
+5 6 7   7
+
+'''
+
 print(ans)
