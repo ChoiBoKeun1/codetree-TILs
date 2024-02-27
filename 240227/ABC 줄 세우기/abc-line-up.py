db7c2cfd6273a0af5ec44d14dff7a808c3987b04
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-27 19:53:37 +0900

    Title: ABC 줄 세우기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/ABC 줄 세우기/abc-line-up.py
+++ 240227/ABC 줄 세우기/abc-line-up.py
@@ -2,12 +2,20 @@ n = int(input())
 arr = list(input().split())
 
 cnt = 0
-for i in range(1,n):
-    for j in range(i,0,-1):
-        if ord(arr[j-1]) > ord(arr[j]):
-            arr[j-1],arr[j] = arr[j],arr[j-1]
-            cnt += 1
-        else:
-            break
+
+for i in range(n):
+    # i번째 알파벳 x 선언.
+    x = chr(ord('A') + i)
+
+    # x 의 현재 idx를 찾는다
+    idx = 0
+    for j in range(n):
+        if arr[j] == x:
+            idx = j
+
+    # x를 i로 옮긴다.
+    for j in range(idx-1, i-1, -1):
+        arr[j],arr[j+1] = arr[j+1],arr[j]
+        cnt += 1
 
 print(cnt)
