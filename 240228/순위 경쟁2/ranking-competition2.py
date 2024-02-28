15912717bce0c1e552b432ecec4e15e44d0e13b2
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-02-28 16:56:40 +0900

    Title: 순위 경쟁2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/순위 경쟁2/ranking-competition2.py
+++ 240228/순위 경쟁2/ranking-competition2.py
@@ -3,11 +3,15 @@ n = int(input())
 a,b = 0,0
 
 def check():
+    # 시작하자마자, a == b 인 경우. 즉 a=0,b=0인 경우. 그냥 넘어간
+    if honor == "" and a == b:
+        return ""
+    
     if a > b:
         return 'A'
     elif a < b:
         return 'B'
-    else:
+    elif a == b:
         return 'AB'
 
 ans = 0
