a06b077a5ae3ffb537e76f8c4922f703ce66a085
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-01 14:46:42 +0900

    Title: 세 수의 최대 곱, Time: 182ms, Memory: 31MB, Status: Passed - CodeTree

--- 240301/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240301/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -3,35 +3,16 @@ import sys
 n = int(input())
 arr = list(map(int,input().split()))
 
-MAX_NUM = -1001
-MIN_NUM = 1001
-for elem in arr:
-    MAX_NUM = max(MAX_NUM, elem)
-    MIN_NUM = min(MIN_NUM, elem)
+arr.sort()
 
-ans = 0
-if n == 3 and 0 in arr:
-    print(0)
-    sys.exit()
+# 가장 큰 수 3개
+# 세 수 +++ --- 의 곱이 최대인 경우
+ans1 = arr[-1] * arr[-2] * arr[-3]
 
-# 모든 숫자가 음수 인 경우
-# 1. 절댓값이 가장 작은거 3개 = 가장 큰 수 3개
-# 2. 0 포함된 경우, 0이 최댓값
-if MAX_NUM == 0:
-    print(0)
-    sys.exit()
-elif MAX_NUM < 0:
-    myarr = sorted(arr)
-    ans = myarr[n-1] * myarr[n-2] * myarr[n-3]
+# 가장 작은 수 2개 + 가장 큰 수 1개
+# 세 수 +-- 의 경우.
+ans2 = arr[0] * arr[1] * arr[-1]
 
-# + + +. 가장큰거 3개
-elif MIN_NUM > 0:
-    myarr = sorted(arr)
-    ans = myarr[n-1] * myarr[n-2] * myarr[n-3]
-# + - -
-# 절댓값이 가장 큰 음수 2개 + 가장 큰 양수 1개
-else:
-    myarr = sorted(arr)
-    ans = myarr[0] * myarr[1] * myarr[-1]
-
+# 둘 중 큰거
+ans = max(ans1,ans2)
 print(ans)
