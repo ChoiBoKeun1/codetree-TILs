592b53ba37f3e687eadb7f5e7eee897b7bff29ed
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-02 15:55:03 +0900

    Title: 최소 와이파이 수, Time: 195ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240302/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,29 +2,14 @@ import sys
 
 n,m = map(int,input().split())
 arr = list(map(int,input().split()))
+wifi = [False] * n
+wifi_range = 2*m + 1
 
-if m == 0:
-    cnt = 0
-    for elem in arr:
-        if elem == 1:
-            cnt += 1
-    print(cnt)
-    sys.exit()
-
-if not 1 in arr:
-    print(0)
-    sys.exit()
-
-if m >= n:
-    print(1)
-    sys.exit()
-
-ans1 = n / (2*m +1)
-ans2 = n // (2*m +1)
-#print(ans1,ans2)
-if ans1 == ans2:
-    print(ans2)
-elif ans1 < ans2 + 0.5:
-    print(ans2)
-else:
-    print(ans2 +1)
+cnt = 0
+for i in range(n):
+    if arr[i] == 1 and not wifi[i]:
+        cnt += 1  
+        for j in range(2*m+1):
+            if i+j < n:
+                wifi[i+j] = True
+print(cnt)
