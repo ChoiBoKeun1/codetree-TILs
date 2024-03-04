014deb76562ca159e8913d1888a1eda2f9607a95
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-04 16:14:15 +0900

    Title: 수열의 순서 바꾸기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/수열의 순서 바꾸기/reorder-sequence.py
+++ 240304/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,18 +1,15 @@
 n = int(input())
 arr = list(map(int,input().split()))
 
-sorted_arr = sorted(arr)
+# 수열이 어느 idx부터 정렬되어있는지를 확인
+# 앞부분에서 정렬되어있지 않은 원소들의 개수가 정답
 
-cnt = 0
+# 수열의 뒤에서부터 확인하면서, 어디까지 정렬되어있는지를 확인
 
-for i in range(n):
-    flag = False
+idx = n-2
+# idx 가 0보다 크고, 정렬이 되어있는지를 확인한다
+while idx >= 0 and arr[idx] < arr[idx+1]:
+    idx -= 1
 
-    for j in range(i,n-1):
-        if arr[j] > arr[j+1]:
-            flag = True
-    
-    if flag:
-        cnt += 1
-
-print(cnt)
+# while문 탈출시, idx+1 개의 원소가 정렬되어있지 않다는 뜻이다.
+print(idx+1)
