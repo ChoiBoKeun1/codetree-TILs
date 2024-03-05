cf027554083dd696d445726caed1936bccc17223
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-05 14:49:00 +0900

    Title: 2개씩 그룹짓기 2, Time: 220ms, Memory: 40MB, Status: Passed - CodeTree

--- 240305/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240305/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -3,20 +3,14 @@ import sys
 n = int(input())
 arr = list(map(int,input().split()))
 
-# 2 5 7 9 10 15
 # 정렬 후 숫자 작은 그룹 반, 큰 그룹 반으로 나눴을때
 # 각 그룹에서 같은 idx끼리 묶는게 가장 최선이다.
+# 즉 arr[0] 과 arr[0+n] 을 묶는것이다
 
-myarr = sorted(arr)
-s_arr = myarr[0:n]
-b_arr = myarr[n:2*n]
-
-differs = []
-for i in range(n):
-    differs.append(b_arr[i] - s_arr[i])
+arr.sort()
 
 ans = sys.maxsize
-for elem in differs:
-    ans = min(ans, elem)
+for i in range(n):
+    ans = min(ans, arr[n+i]-arr[i])
 
 print(ans)
