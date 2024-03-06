636795fead5d638487088459673e5476066dbe86
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-06 14:27:57 +0900

    Title: A, B, C 찾기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/A, B, C 찾기/finding-a-b-c.py
+++ 240306/A, B, C 찾기/finding-a-b-c.py
@@ -2,22 +2,13 @@ arr = list(map(int,input().split()))
 arr.sort()
 '''
 a <= b <= c
-1. 2a <= a+b <= c+a
-2. a+b <= 2b <= b+c
-3. a+c <= b+c <= 2c
-
-1+3.
-a+b <= c+a <= b+c
-
-a+b <= c+a <= b+c <= a+b+c
 
 가장 작은 숫자는 a.
 그 다음으로 작은 숫자는 b.
-
-그 다음으로 작은 숫자는 c 또는 a+b
+가장 큰 숫자는 a+b+c
 '''
-a,b,c = arr[0],arr[1],arr[2]
-if a+b+c != arr[-1]:
-    c = arr[3]
+
+a,b = arr[0],arr[1]
+c = arr[-1] - (a+b)
 
 print(a,b,c)
