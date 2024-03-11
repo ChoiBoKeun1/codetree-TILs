f2e223583e663efe5307c6583485fc3ce9d8cfea
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-11 15:51:13 +0900

    Title: 기수 정렬 구현, Time: 143ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240311/기수 정렬 구현/implement-radix-sort.py
+++ 240311/기수 정렬 구현/implement-radix-sort.py
@@ -1,4 +1,7 @@
+# 1 <= 원소 값 <= 100000 이므로, 최대 자릿수 개수는 6
 MAX_K = 6
+
+# 숫자니까 0 ~ 9 10개.
 MAX_DIGIT = 10
 
 n = int(input())
@@ -9,21 +12,18 @@ def radix_sort():
 
     p = 1
     for pos in range(MAX_K):
-        new_arr = [
-            [] for _ in range(MAX_DIGIT)
-        ]
+        new_arr = [ [] for _ in range(MAX_DIGIT) ]
 
         for elem in arr:
             digit = (elem // p) % 10
             new_arr[digit].append(elem)
-        
+
         arr = []
-        for digit in range(MAX_DIGIT):
-            for elem in new_arr[digit]:
-                arr.append(elem)
+        for i in range(MAX_DIGIT):
+            for j in range(len(new_arr[i])):
+                arr.append(new_arr[i][j])
 
-        p *= 10
-
+    p *= 10
 
 radix_sort()
 
