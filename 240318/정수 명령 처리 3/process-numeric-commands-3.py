63ce76ed290d08c2aa9eb5ab5a52d92efa78f9ba
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-03-18 23:56:03 +0900

    Title: 정수 명령 처리 3, Time: 320ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240318/정수 명령 처리 3/process-numeric-commands-3.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+dq = deque()
+
+n = int(input())
+for _ in range(n):
+    cmd = input().split()
+
+    if cmd[0] == 'push_front':
+        dq.appendleft(cmd[1])
+    
+    elif cmd[0] == 'push_back':
+        dq.append(cmd[1])
+
+    elif cmd[0] == 'pop_front':
+        print(dq.popleft())
+
+    elif cmd[0] == 'pop_back':
+        print(dq.pop())
+
+    elif cmd[0] == 'size':
+        print(len(dq))
+
+    elif cmd[0] == 'empty':
+        if dq:
+            print(0)
+        else:
+            print(1)
+
+    elif cmd[0] == 'front':
+        print(dq[0])
+
+    elif cmd[0] == 'back':
+        print(dq[-1])
