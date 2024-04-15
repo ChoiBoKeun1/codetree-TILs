c67e50fcb4c94b2195043c284a6150045922c3c9
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-04-15 13:02:38 +0900

    Title: 기울어진 직사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240415/기울어진 직사각형/slanted-rectangle.py
+++ 240415/기울어진 직사각형/slanted-rectangle.py
@@ -4,47 +4,36 @@ arr = [
     for _ in range(n)
 ]
 
-dx = [-1,-1,1,1]
-dy = [1,-1,-1,1]
-
-ans = 0
-
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def simulate(x,y,a,b):
-    global ans
-    
+def get_score(x,y,a,b):
+    dxs = [-1,-1,1,1]
+    dys = [1,-1,-1,1]
+
+    move_nums = [a,b,a,b]
+
     sum_val = 0
-    is_possible = True
 
-    l = [a,b,a,b]
+    # 기울어진 직사각형의 경계를 쭉 따라가기
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            x,y = x+dx, y+dy
 
-    for c in range(4):
-        if not is_possible:
-            break
-        
-        cur_len = l[c]
-
-        for d in range(cur_len):
-            nx = x + dx[c]
-            ny = y + dy[c]
-
-            if in_range(nx,ny):
-                sum_val += arr[nx][ny]
-                x,y = nx,ny
-
-            else:
-                is_possible = False
-                break
-
-    if is_possible:
-        ans = max(ans, sum_val)
+            # 기울어진 직사각형이 경계를 벗어나는 경우
+            # 불가능하다는 의미로, 0을 return.
+            if not in_range(x,y):
+                return 0
+            
+            sum_val += arr[x][y]
+    
+    return sum_val
 
 
 for i in range(n):
     for j in range(n):
         for a in range(1,n):
             for b in range(1,n):
-                simulate(i,j,a,b)
+                ans = max(ans, get_score(i,j,a,b))
+
 print(ans)
