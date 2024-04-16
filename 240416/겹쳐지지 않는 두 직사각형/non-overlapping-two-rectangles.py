43f1b50209a994a06ea1ea9bb84631f524b5e9ec
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-04-16 13:57:06 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 908ms, Memory: 29MB, Status: Passed - CodeTree

--- 240416/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240416/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -6,35 +6,73 @@ arr = [
     for _ in range(n)
 ]
 
-def get_sum(x,y, w,h):
-    cur = 0
-    for i in range(x, x+h +1):
-        for j in range(y, y+w +1):
-            cur += arr[i][j]
-    
-    return cur
+# 빈 board
+board = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+def clear_board():
+    for i in range(n):
+        for j in range(m):
+            board[i][j] = 0
+
+def draw(x1,y1,x2,y2):
+    for i in range(x1, x2+1):
+        for j in range(y1, y2+1):
+            board[i][j] += 1
+
+def check_board():
+    # 동일한 칸을 2개의 직사각형이 모두 포함한다면
+    # 겹친다. True 리턴.
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] >= 2:
+                return True
+    return False
+
+# 두 직사각형이 겹치는지 확인한다
+# 겹치면 True. 안겹치면 False
+def overlapped(x1,y1,x2,y2, x3,y3, x4,y4):
+    clear_board()
+    draw(x1,y1, x2,y2)
+    draw(x3,y3, x4,y4)
+    return check_board()
+
+# 직사각형 합
+def rect_sum(x1,y1, x2,y2):
+    return sum([
+        arr[i][j]
+        for i in range(x1, x2+1)
+        for j in range(y1, y2+1)
+    ])
+
+# 첫 직사각형 x1,y1,x2,y2 가 주어졌을 때,
+# 두번째 직사각형(i,j,k,l)을 잘 잡아서
+# 최대 합을 반환한다.
+def find(x1,y1,x2,y2):
+    max_sum = -sys.maxsize
+
+    # 가능한 모든 직사각형
+    for i in range(n):
+        for j in range(m):
+            for k in range(i,n):
+                for l in range(j,m):
+                    # 두 직사각형이 겹치지 않을 때,
+                    # 합을 구한다
+                    if not overlapped(x1,y1,x2,y2, i,j,k,l):
+                        max_sum = max(max_sum, rect_sum(x1,y1,x2,y2) + rect_sum(i,j,k,l))
+    return max_sum
 
 ans = -sys.maxsize
 
-# rectangle1의 왼쪽위 점 i,j
+# (i,j), (k,l)을 양쪽 꼭지점으로 하는
+# 첫번째 직사각형을 정하여
+# 그 중 최댓값을 찾아 반환
 for i in range(n):
     for j in range(m):
-        # rectangle1 가로,세로길이
-        for w1 in range(m):
-            for h1 in range(n):
-                if i+h1 >= n or j+w1 >= m:
-                    continue
-                
-                for k in range(n):
-                    for l in range(m):
-                        for w2 in range(m):
-                            for h2 in range(n):
-                                if k+h2 >= n or l+w2 >= m:
-                                    continue
-                                
-                                if i+h1 < k or j+w1 < l:
-                                    rectangle1 = get_sum(i,j,w1,h1)
-                                    rectangle2 = get_sum(k,l,w2,h2)
-                                    ans = max(ans, rectangle1+rectangle2)
+        for k in range(i, n):
+            for l in range(j, m):
+                ans = max(ans, find(i,j,k,l))
 
 print(ans)
