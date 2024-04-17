9b54ee9e461a221064251728db84580fa6266eed
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-04-17 12:36:49 +0900

    Title: 양수 직사각형의 최대 크기, Time: 215ms, Memory: 26MB, Status: Passed - CodeTree

--- 240417/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240417/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -8,7 +8,7 @@ arr = [
 def is_positive_rect(x1,y1, x2,y2):
     for i in range(x1,x2+1):
         for j in range(y1,y2+1):
-            if arr[i][j] < 0:
+            if arr[i][j] <= 0:
                 return False
     
     return True
