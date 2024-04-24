3a2a09d2fc41b4743fe4ecb003de7c701f93117b
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-04-24 12:16:35 +0900

    Title: 1차원 젠가, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/1차원 젠가/jenga-1d.py
@@ -0,0 +1,26 @@
+n = int(input())
+tower = [0] + [
+    int(input())
+    for _ in range(n)
+]
+
+def remove(tower, start, end):
+    tmp = [0]
+    for i in range(1,len(tower)):
+        if start <= i and i <= end:
+            continue
+        
+        tmp.append(tower[i])
+
+    return tmp
+
+
+s1,e1 = map(int,input().split())
+tower = remove(tower, s1,e1)
+
+s2,e2 = map(int,input().split())
+tower = remove(tower, s1,e2)
+
+print(len(tower)-1)
+for i in range(1,len(tower)):
+    print(tower[i])
