970eaa07764743df53e9cfde3e89851aa65cdd48
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-04-25 13:47:46 +0900

    Title: 십자 모양 폭발, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,52 @@
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+def print_arr(arr):
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j],end=' ')
+        print()
+
+def in_range(x,y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def bomb(r,c):
+    num = arr[r][c]
+
+    dxs = [1,0,-1,0]
+    dys = [0,1,0,-1]
+
+    arr[r][c] = 0
+    
+    for dx, dy in zip(dxs,dys):
+        for _ in range(num):
+            cur_x = r + dx
+            cur_y = c + dy
+            
+            if in_range(cur_x,cur_y):
+                arr[cur_x][cur_y] = 0
+
+def shift(arr):
+    tmp = [
+        [0] * n
+        for _ in range(n)
+    ]
+
+    for j in range(n):
+        
+        tmp_row = n-1
+        for i in range(n-1,-1,-1):
+            if arr[i][j] != 0:
+                tmp[tmp_row][j] = arr[i][j]
+                tmp_row -= 1
+    return tmp            
+
+
+r,c = map(int,input().split())
+r,c = r-1,c-1
+
+bomb(r,c)
+print_arr(shift(arr))
