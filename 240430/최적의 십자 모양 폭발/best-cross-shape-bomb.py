bb53f65a36e57abca5395de27d3c75045a8b373c
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-04-30 15:36:16 +0900

    Title: 최적의 십자 모양 폭발, Time: 204ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240430/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -0,0 +1,72 @@
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+def in_range(x,y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+def fall(arr):
+    tmp = [
+        [0] * n
+        for _ in range(n)
+    ]
+
+    for j in range(n):
+        cur_idx = n-1
+        for i in range(n-1,-1,-1):
+            if arr[i][j] != 0:
+                tmp[cur_idx][j] = arr[i][j]
+                cur_idx -= 1
+
+    return tmp
+
+def boom(r,c):
+    tmp = [
+        inner[:]
+        for inner in arr
+    ]
+
+    dxs = [1,0,-1,0]
+    dys = [0,1,0,-1]
+    
+    bomb_range = arr[r][c]
+
+    tmp[r][c] = 0
+    
+    for dx, dy in zip(dxs,dys):
+        cur_x, cur_y = r,c
+
+        for _ in range(bomb_range-1):
+            cur_x += dx
+            cur_y += dy
+
+        if in_range(cur_x, cur_y):
+            tmp[cur_x][cur_y] = 0
+
+
+    fallen_arr = fall(tmp)
+    return fallen_arr
+
+def check_adj(arr):
+    cnt = 0
+
+    for i in range(n):
+        for j in range(n-1):
+            if arr[i][j] != 0 and arr[i][j] == arr[i][j+1]:
+                cnt += 1
+
+    for j in range(n):
+        for i in range(n-1):
+            if arr[i][j] != 0 and arr[i][j] == arr[i+1][j]:
+                cnt += 1
+
+    return cnt
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        ans = max(ans,check_adj(boom(i,j)))
+
+print(ans)
