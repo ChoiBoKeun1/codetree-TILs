4516c15df6869825593fd7a54b60210e3d4e340e
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-04 11:22:23 +0900

    Title: 주사위 던지기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240504/주사위 던지기/roll-a-dice.py
+++ 240504/주사위 던지기/roll-a-dice.py
@@ -6,56 +6,62 @@ arr = [
     for _ in range(n)
 ]
 
-dice = [
-    [0,5,0],
-    [4,6,3],
-    [0,2,0]
-]
+moves = input().split()
 
-def cur_eye():
-    return dice[1][1]
+# 윗면, 앞면, 오른쪽면
+up,front,right = 1,2,3
 
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def roll(d):
-    if d == 'L':
-        dice[1] = [7 - cur_eye(), dice[1][0], dice[1][1]]
-    elif d == 'R':
-        dice[1] = [dice[1][1], dice[1][2], 7 - cur_eye()]
-    
-    elif d == 'U':
-        [dice[0][1],dice[1][1],dice[2][1]] = [7-cur_eye(), dice[0][1], dice[1][1]]
-
-    elif d == 'D':
-        [dice[0][1],dice[1][1],dice[2][1]] = [dice[1][1], dice[2][1], 7-cur_eye()]
-
-arr[r][c] = cur_eye()
-d_list = map(str,input().split())
-
-for d in d_list:
-    if d == 'L':
-        direction = 0
-    elif d == 'R':
-        direction = 1
-    elif d == 'U':
-        direction = 2
-    else:
-        direction = 3
-
-    # L,R,U,D
+# 다음 위치를 구한다
+def next_pos(x,y, move_dir):
+    # R,L,U,D 순서
     dxs = [0,0,-1,1]
-    dys = [-1,1,0,0]
+    dys = [1,-1,0,0]
 
-    next_x = r + dxs[direction]
-    next_y = c + dys[direction]
+    nx = x + dxs[move_dir]
+    ny = y + dys[move_dir]
 
-    if not in_range(next_x,next_y):
-        continue
+    if in_range(nx,ny):
+        return (nx,ny)
+    else:
+        (-1,-1)
 
-    r = next_x
-    c = next_y
-    roll(d)
-    arr[r][c] = cur_eye()
+def simulate(move_dir):
+    global r,c
+    global up,front,right
+
+    nx,ny = next_pos(r,c,move_dir)
+
+    if (nx,ny) == (-1,-1):
+        return
+    
+    r,c = nx,ny
+
+    # R,L,U,D
+    if move_dir == 0:
+        up,front,right = 7 - right, front, up
+    elif move_dir == 1:
+        up,front,right = right, front, 7 - up
+    elif move_dir == 2:
+        up,front,right = front, 7 - up, right
+    else:
+        up,front,right = 7 - front, up, right
+
+    bottom = 7 - up
+    arr[r][c] = bottom
+
+dir_mapper = {
+    'R': 0,
+    'L': 1,
+    'U': 2,
+    'D': 3
+}
+
+# main
+arr[r][c] = 6
+for move_dir in moves:
+    simulate(dir_mapper[move_dir])
 
 print(sum(map(sum, arr)))
