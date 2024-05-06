478a74ac87af096179c812b818922fa4ba9c5cc7
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-06 14:48:55 +0900

    Title: 뱀은 사과를 좋아해, Time: 672ms, Memory: 26MB, Status: Passed - CodeTree

--- 240506/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 240506/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -1,98 +1,104 @@
-from collections import deque
 n,m,k = map(int,input().split())
+apple = [
+    [False for _ in range(n+1)]
+    for _ in range(n+1)
+]
 
-#몸통만 뱀 
-one_maps = [ [0]*n for _ in range(n)]
+# 뱀. snake[0]에는 항상 머리가 있다.
+snake = [(1,1)]
+total_time = 0
 
-#머리만 이동뱀
-head_maps = [ [0]*n for _ in range(n)]
+dir_mapper = {
+    'D': 0,
+    'R': 1,
+    'U': 2,
+    'L': 3
+}
 
-#뱀들의 머리 정보 0인덱스가 머리 그다음부터 몸 
+# x,y가 범위 안에 있는가
+def in_range(x,y):
+    return 0 < x and x <= n and 0 < y and y <= n
 
-snake = deque()
-dxs=[-1,1,0,0]
-dys=[0,0,1,-1]
-#사과 위치
-apple = [ [0]*n for _ in range(n)]
+# 뱀이 꼬였는지 확인
+def is_twisted(new_head):
+    return new_head in snake
 
+# 새 머리 추가
+def push_front(new_head):
+    if is_twisted(new_head):
+        return False
 
-for j in range(m) : 
-    x,y= map(int,input().split())
-    x-=1
-    y-=1
-    apple[x][y] = 1 
+    snake.insert(0, new_head)
 
-start_x = 0
-start_y = 0
-snake.append((start_x,start_y))
+    return True
 
-def in_range(x,y) : 
-    return 0<= x < n and 0<=y<n
+# 꼬리 지우기
+def pop_back():
+    snake.pop()
 
-# 다음 위치에 뱀머리, 뱀몸통을 업데이트 시킴 
-def update_snake(snake) : 
-    global one_maps, head_maps
-    one_maps = [ [0]*n for _ in range(n)] #맵스 초기화 
-    head_maps = [ [0]*n for _ in range(n)] #맵스 초기화 
-    for i in range(len(snake)) : 
-        a,b = snake[i]
-        if i == 0 : 
-            head_maps[a][b] = 2 # 2가 머리 
-        else : 
-            one_maps[a][b] = 1 # 그다음 녀석을 그린다. 
-    for x in range(n) : 
-        for y in range(n) : 
-            if in_body(x,y) : 
-                return -1 
+# 뱀 움직이기
+def move_snake(nx,ny):
+    # 움직일 위치에 사과가 있다면
+    # 사과 먹고
+    # 머리 추가, 꼬리 삭제 X
+    if apple[nx][ny]:
+        # 사과 먹기
+        apple[nx][ny] = False
 
+        # 머리 추가.
+        # 늘어난 머리때문에 몸이 꼬이면 False 리턴
+        if not push_front((nx,ny)):
+            return False
 
-def in_body(x,y) : 
-    if head_maps[x][y] ==2 and one_maps[x][y] == 1  : 
-        return True
-    return False
+    # 사과가 없으면
+    # 머리 추가, 꼬리 삭제
+    else:
+        # 꼬리 삭제
+        pop_back()
 
-# 뱀 이동 위치 
-def move(pos):
-    #x,y 는 헤드값이다. 
-    x,y = snake[0]
-    
-    nx,ny = x + dxs[pos], y + dys[pos]
-    if not in_range(nx,ny) : 
-        return -1 # 격자를 벗어난다. 
-    if in_range(nx,ny)  :
-        if apple[nx][ny] == 1 : # 사과가있다면 
-            apple[nx][ny] = 0 # 0으로 하고
-            # snake 길이 그냥 늘리기 
-            snake.appendleft((nx,ny))
-        else : # 아닐경우 
-            snake.appendleft((nx,ny)) # 머리로 등록 .
-            snake.pop() #꼬리 자르기 
-    #옮기고 난다음, 
-    return 0
+        # 머리 추가.
+        # 늘어난 머리때문에 몸이 꼬이면 False 리턴
+        if not push_front((nx,ny)):
+            return False
 
-times = 0
-stop_count = False
-checked2 = 0
+    return True
 
-for _ in range(k) :
-    d,p = input().split() 
-    if d == 'U' : 
-        pos = 0
-    elif d=='D' : 
-        pos = 1 
-    elif d == 'R' : 
-        pos = 2 
-    else : 
-        pos = 3 
+# move_dir 방향으로 num번 움직인다
+def move(move_dir, num):
+    global total_time
 
+    dxs = [1,0,-1,0]
+    dys = [0,1,0,-1]
+
+    for _ in range(num):
+        total_time += 1
+
+        x,y = snake[0]
+
+        nx = x + dxs[move_dir]
+        ny = y + dys[move_dir]
+
+        # 다음 위치로 갈수 없다면
+        # 게임 종료
+        if not in_range(nx,ny):
+            return False
+
+        # 뱀을 한칸 움직인다
+        # 움직였는데 몸이 꼬였다면, 게임 종료
+        if not move_snake(nx,ny):
+            return False
+
+    return True
+
+for _ in range(m):
+    x,y = map(int,input().split())
+    apple[x][y] = True
+
+for _ in range(k):
+    d,p = input().split()
     p = int(p)
-    for power in range(p) : 
-        times += 1 
-        checked = move(pos) 
-        checked2 = update_snake(snake)
-        if checked == -1 or checked2 == -1 : #끝내기 
-            stop_count = True
-            break
-    if stop_count == True : 
+
+    if not move(dir_mapper[d],p):
         break
-print(times)
+
+print(total_time)
