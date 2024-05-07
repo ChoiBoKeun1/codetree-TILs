2aae7dfe33877e1ecead3872eda453968c7df554
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-07 14:24:06 +0900

    Title: 핀볼게임, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/핀볼게임/pinball-game.py
+++ 240507/핀볼게임/pinball-game.py
@@ -1,77 +1,46 @@
 n = int(input())
-
 arr = [
     list(map(int,input().split()))
     for _ in range(n)
 ]
 
-total_time = 0
-
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-def simulate(x,y, dx,dy):
-    global total_time
-    total_time = 1
+def simulate(x,y, cur_dir):
+    # 상,하,좌,우
+    dxs = [-1,1,0,0]
+    dys = [0,0,-1,1]
+    # / 만나면 : 0<->3 , 1<->2
+    # \ 만나면 : 0<->2 , 1<->3
 
-    if arr[x][y] == 1:
-        dx,dy = -dy, -dx
-    elif arr[x][y] == 2:
-        dx,dy = dy,dx
+    total_time = 1
     
-    while True:
+    while in_range(x,y):
         total_time += 1
 
-        nx = x + dx
-        ny = y + dy
-
-        if not in_range(nx,ny):
-            break
-
         # / 를 만난 경우
-        # 방향이 꺾이면서 부호도 달라져야 한다
-        if arr[nx][ny] == 1:
-            dx,dy = -dy, -dx
+        if arr[x][y] == 1:
+            cur_dir = 3 - cur_dir
         
         # \ 를 만난 경우
-        # 방향이 꺾이지만 부호는 그대로
-        elif arr[nx][ny] == 2:
-            dx,dy = dy,dx
+        elif arr[x][y] == 2:
+            cur_dir = (cur_dir + 2) % 4
 
-        x,y = nx,ny
+        x = x + dxs[cur_dir]
+        y = y + dys[cur_dir]
+
+    return total_time
 
 ans = 0
 
-# 위에서 아래로
-for j in range(n):
-    start_x,start_y = 0,j
-    dx,dy = 1,0
-    
-    simulate(start_x,start_y, dx,dy)
-    ans = max(ans,total_time)
-
-# 왼쪽에서 오른쪽으로
 for i in range(n):
-    start_x,start_y = i,0
-    dx,dy = 0,1
+    # 상하좌우 순서. 
+    # 상 : 아래에서 위로 올라간다는 뜻
 
-    simulate(start_x,start_y,dx,dy)
-    ans = max(ans,total_time)
-
-# 왼쪽에서 오른쪽으로
-for i in range(n):
-    start_x,start_y = i,n-1
-    dx,dy = 0,-1
-
-    simulate(start_x,start_y,dx,dy)   
-    ans = max(ans,total_time)
-
-# 아래에서 위로
-for j in range(n):
-    start_x,start_y = n-1,j
-    dx,dy = -1,0
-
-    simulate(start_x,start_y,dx,dy)
-    ans = max(ans,total_time)
+    ans = max(ans, simulate(n-1,i,0))
+    ans = max(ans, simulate(0,i,1))
+    ans = max(ans, simulate(i,n-1,2))
+    ans = max(ans, simulate(i,0,3))
 
 print(ans)
