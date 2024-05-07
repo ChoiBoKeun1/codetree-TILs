8904a8f19518177b6992809a6c28c08af2251e06
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-07 14:13:42 +0900

    Title: 핀볼게임, Time: 147ms, Memory: 26MB, Status: Passed - CodeTree

--- 240507/핀볼게임/pinball-game.py
+++ 240507/핀볼게임/pinball-game.py
@@ -5,26 +5,6 @@ arr = [
     for _ in range(n)
 ]
 
-'''
-# 입구 순서
-(0,         0 1 2 3 4)
-(0 1 2 3 4, 4)
-(4,         0 1 2 3 4)
-(0 1 2 3 4, 0)
-
-for j in range(n):
-    arr[0][j]
-
-for i in range(n):
-    arr[i][n]
-
-for j in range(n):
-    arr[n][j]
-
-for i in range(n):
-    arr[i][0]
-'''
-
 total_time = 0
 
 def in_range(x,y):
@@ -32,8 +12,13 @@ def in_range(x,y):
 
 def simulate(x,y, dx,dy):
     global total_time
-    total_time = 0
+    total_time = 1
 
+    if arr[x][y] == 1:
+        dx,dy = -dy, -dx
+    elif arr[x][y] == 2:
+        dx,dy = dy,dx
+    
     while True:
         total_time += 1
 
@@ -62,7 +47,7 @@ for j in range(n):
     start_x,start_y = 0,j
     dx,dy = 1,0
     
-    simulate(start_x,start_y,dx,dy)
+    simulate(start_x,start_y, dx,dy)
     ans = max(ans,total_time)
 
 # 왼쪽에서 오른쪽으로
@@ -75,7 +60,7 @@ for i in range(n):
 
 # 왼쪽에서 오른쪽으로
 for i in range(n):
-    start_x,start_y = i,n
+    start_x,start_y = i,n-1
     dx,dy = 0,-1
 
     simulate(start_x,start_y,dx,dy)   
@@ -83,12 +68,10 @@ for i in range(n):
 
 # 아래에서 위로
 for j in range(n):
-    start_x,start_y = n,j
+    start_x,start_y = n-1,j
     dx,dy = -1,0
 
     simulate(start_x,start_y,dx,dy)
     ans = max(ans,total_time)
 
-# simulate(4,0,1)
-# print(total_time)
 print(ans)
