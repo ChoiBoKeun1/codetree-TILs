fce590943d65f442d4ed7075cfb535ef99892475
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-11 12:18:00 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240511/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
+++ 240511/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -1,75 +1,82 @@
-import sys
+# 입력
+n,m = map(int,input().split())
+arr = [
+    [[] for _ in range(n)]
+    for _ in range(n)
+]
 
-n, m = tuple(map(int, input().split()))
-arr = [[[int(num)] for num in input().split()] for _ in range(n)]
+def get_pos(move_num):
+    for i in range(n):
+        for j in range(n):
+            for num in arr[i][j]:
+                if num == move_num:
+                    return (i,j)
 
-move_arr = list(map(int, input().split()))
+def in_range(x,y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-dxs = [-1, -1, 0, 1, 1, 1, 0, -1]
-dys = [0, 1, 1, 1, 0, -1, -1, -1]
+# 다음 위치 반환
+def next_pos(pos):
+    dxs = [-1,-1,-1,0,0,1,1,1]
+    dys = [-1,0,1,-1,1,-1,0,1]
 
-# 격자 내에 있는지 체크
-def is_in_range(next_row, next_col) :
-    return 0 <= next_row < n and 0 <= next_col < n
+    x,y = pos
 
-# 숫자 이동하기
-def move_cur_num(a, b, c, max_row, max_col, max_num, move_num) :
-    temp_arr = []
+    max_val = -1
+    max_pos = (-1,-1)
 
-    if max_num != 0 :
-        for e in arr[a][b][ : arr[a][b].index(move_num) + 1] :
-            temp_arr.append(e)
-            arr[a][b].remove(e)
+    for dx,dy in zip(dxs,dys):
+        nx = x + dx
+        ny = y + dy
 
-    for e in arr[max_row][max_col] :
-        temp_arr.append(e)
+        if in_range(nx,ny):
+            for num in arr[nx][ny]:
+                if num > max_val:
+                    max_val = num
+                    max_pos = (nx,ny)
 
-    arr[max_row][max_col] = temp_arr
+    return max_pos
 
-    if len(arr[a][b]) == 0 :
-        arr[a][b].append(0)
-
-    return arr
-
-# 8방향 중에서 가장 큰 수가 있는지 체크
-def find_the_biggest_num(row, col, point) :
-    max_num = -sys.maxsize
-    tar_idx = -1
-    global move_num
+def move(pos, next_pos, move_num):
+    x,y = pos
+    nx,ny = next_pos
     
-    for dx, dy in zip(dxs, dys) :
-        next_row = row + dx
-        next_col = col + dy
+    # 1. (x,y)의 move_num 위에 있는 숫자들을 
+    #    (nx,ny) 로 옮긴다
+    to_move = False
+    for num in arr[x][y]:
+        if num == move_num:
+            to_move = True
+        
+        if to_move:
+            arr[nx][ny].append(num)
 
-        if is_in_range(next_row, next_col) :
-            for e in arr[next_row][next_col] :
-                if e >= max_num :
-                    max_num = e
-                    max_row = next_row
-                    max_col = next_col
-                    tar_idx = arr[next_row][next_col].index(e)
-    return max_row, max_col, tar_idx, max_num
+    # 2. (x,y)에 있던 숫자들 중
+    #    움직인 숫자들을 전부 지운다
+    while arr[x][y][-1] != move_num:
+        arr[x][y].pop()
+    arr[x][y].pop()
 
-# 움직일 숫자의 위치 찾기
-def find_point(arr, move_num) :
-    for a in range(len(arr)) :
-        for b in range(len(arr[a])) :
-            for c in range(len(arr[a][b])) :
-                if arr[a][b][c] == move_num :
-                    return a, b, c
+def simulate(move_num):
+    pos = get_pos(move_num)
+    max_pos = next_pos(pos)
+    if max_pos != (-1,-1):
+        move(pos, max_pos, move_num)
 
-for i in range(len(move_arr)) :
-    a, b, c = find_point(arr, move_arr[i])
-    if move_arr[i] == arr[a][b][c] :
-        # 8방향 중에서 제일 큰 숫자의 위치 리턴
-        max_row, max_col, tar_idx, max_num = find_the_biggest_num(a, b, c) 
-        arr = move_cur_num(a, b, c, max_row, max_col, max_num, move_arr[i])
+for i in range(n):
+    given_row = list(map(int,input().split()))
+    for j, num in enumerate(given_row):
+        arr[i][j].append(num)
 
-for e in arr :
-    for ee in e :
-        for eee in ee :
-            if eee == 0 :
-                print('None', end = ' ')
-            else :
-                print(eee, end = ' ')
+move_nums = list(map(int,input().split()))
+for move_num in move_nums:
+    simulate(move_num)
+
+for i in range(n):
+    for j in range(n):
+        if not arr[i][j]:
+            print("None",end='')
+        else:
+            for num in arr[i][j][::-1]:
+                print(num, end=' ')
         print()
