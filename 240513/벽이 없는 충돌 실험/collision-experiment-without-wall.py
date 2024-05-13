fe9009ad064617391dc067bf39772c2935d08fdb
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-13 21:32:20 +0900

    Title: 벽이 없는 충돌 실험, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240513/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 240513/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,124 +1,137 @@
+t = int(input())
+
 COORD_SIZE = 4000
 OFFSET = 2000
 BLANK = -1
 
-# 테스트케이스 개수 입력
-t = int(input())
+'''
+marble = (x, y, weight, d, num)
+'''
 
 marbles = []
 next_marbles = []
-
-# 겹치는 구슬 확인을 위한 2차원 배열
-# 구슬이 겹치는지 확인을 O(1) 에 가능
 next_marble_idx = [
-    [BLANK] * (COORD_SIZE+1)
-    for _ in range(COORD_SIZE+1)
+    [BLANK] * (COORD_SIZE +1)
+    for _ in range(COORD_SIZE +1)
 ]
 
 cur_time = 0
 last_collision_time = -1
 
-mapper = {'U': 0, 'R': 1, 'L': 2, 'D': 3}
+mapper = {'U': 0, 'R': 1, 'D': 2, 'L': 3}
 
-def in_range(x,y):
+def in_range(marble):
+    x,y,_,_,_ = marble
     return 0 <= x and x <= COORD_SIZE and 0 <= y and y <= COORD_SIZE
 
-# 구슬 움직이기
-# 1초 후 구슬 위치 구하기
-def move(marble):
-    dxs = [0,1,-1,0]
-    dys = [1,0,0,-1]
+# 구슬이 움직인 위치에 이미 구슬이 있는지를 확인
+# 있다면 해당 구슬의 idx, 없으면 BLANK가 리턴된다.
+def get_idx_duplicate_marble(marble):
+    x, y, _, _, _ = marble
+    return next_marble_idx[x][y]
 
+# 구슬 2개가 충돌했을 때
+def collision(marble1, marble2):
+    _, _, w1, _, num1 = marble1
+    _, _, w2, _, num2 = marble2
+
+    if w1 > w2 or (w1 == w2 and num1 > num2):
+        return marble1
+    else:
+        return marble2
+
+def move(marble):
+    # 지금까지 썼던 2차원 arr의 row,col을 생각하지 말고,
+    # 좌표평면을 생각했을 때의 dxs, dys. 즉 y축이 위아래, x축이 좌우
+    dxs = [0,1,0,-1]
+    dys = [1,0,-1,0]
+    
     x, y, w, move_dir, num = marble
+
     nx = x + dxs[move_dir]
     ny = y + dys[move_dir]
 
-    return (nx,ny,w,move_dir,num)
+    return (nx, ny, w, move_dir, num)
 
-# marble 위치에 구슬이 이미 있는지 확인
-# 이미 있다면, 그 구슬의 idx 리턴.
-# 없으면, BLANK 리턴 된다.
-def find_duplicate_marble(marble):
-    x,y,_,_,_ = marble
-    return next_marble_idx[x][y]
+# 움직인 구슬이 충돌했는지 확인
+def push_to_next_marbles(marble):
+    global cur_time, last_collision_time
 
-# 두 구슬 충돌시
-# 구슬 하나만 남긴다
-# 우선순위 : weight, num
-def collide(marble1, marble2):
-    _, _, weight1, _, num1 = marble1
-    _, _, weight2, _, num2 = marble2
+    x, y, _, _, _ = marble
 
-    if weight1 > weight2 or (weight1 == weight2 and num1 > num2):
-        return marble1
-    else: 
-        return marble2
-
-# 
-def push_next_marble(marble):
-    global last_collision_time
-
-    x,y, _, _, _ = marble
-
-    # 이 구슬이 범위를 벗어나면,
-    # 함수 종료  
     if not in_range(x,y):
         return
 
-    idx = find_duplicate_marble(marble)
+    # 움직인 구슬의 위치에 이미 구슬이 있는지 확인
+    idx = get_idx_duplicate_marble(marble)
 
-    # 1. 같은 위치에 구슬이 없다면
-    # 그대로 next_marbles 리스트에 추가
+    # 1. 없으면, next_marbles 리스트에 이 구슬을 넣고
+    # 이 구슬이 next_marbles 리스트의 몇번째 index인지를 확인하기 위해
+    # next_marble_idx[x][y]에 index를 넣는다.
     if idx == BLANK:
         next_marbles.append(marble)
-
-        # 겹치는 구슬 확인을 위한
-        # next_marble_idx 2차원 배열에 저장
-        # [x][y] 에 index를 저장
         next_marble_idx[x][y] = len(next_marbles) -1
 
-    # 2. 같은 위치에 구슬이 있다면
-    # 더 영향력 있는 구슬만 남기고
-    # last_collision_time을 cur_time으로 update
+    # 2. 있다면, 더 영향력있는 구슬만 남기고,
+    # 가장 최근 충돌 시간을 업데이트 한다.
     else:
-        next_marbles[idx] = collide(next_marbles[idx], marble)
+        next_marbles[idx] = collision(marble, next_marbles[idx])
         last_collision_time = cur_time
 
+# 모든 구슬을 1번 움직인다.
 def simulate():
     global marbles, next_marbles
 
+    # 모든 구슬에 대해 for문
     for marble in marbles:
-        # 1. 각 구슬에 대해, 한칸 움직인 이후 위치를 가져온다
+        # 1. 구슬을 움직이고
         next_marble = move(marble)
-
-        # 2. next_marbles 리스트 업데이트
-        push_next_marble(next_marble)
+        
+        # 2. 움직인 구슬들에 대해 충돌을 확인 후 처리
+        push_to_next_marbles(next_marble)
     
+    # 3. marbles 리스트를 움직인 후로 업데이트
     marbles = next_marbles[:]
 
-    # next_marbles, next_marble_idx 배열 초기화
+    # 4. 사용한 next_marbles, next_marble_idx 리스트 초기화
     for x,y,_,_,_ in next_marbles:
-        next_marble_idx[x][y] = BLANK
+        next_marbles_idx[x][y] = BLANK
     next_marbles = []
 
+# main함수
 for _ in range(t):
-    marbles = []
-    last_collision_time = -1
-
     n = int(input())
+
     for i in range(1, n+1):
         x,y,w,d = input().split()
         x,y,w = map(int, [x,y,w])
 
-        x,y = 2*x,2*y
+        # 문제에서 주어진 조건 : 
+        # 2초에 한번씩 움직임 + 중간에 만나도 충돌함
+        # 위 2가지 문제를 해결하기 위해
+        # 좌표평면을 2배로 늘리고, 1초에 한번씩 움직이는 문제로 바꿔준다 
 
-        x += OFFSET
-        y += OFFSET
+        # 그것을 위한 주어진 좌표 2배
+        x,y = 2*x, 2*y
 
+        
+        # 또한 주어진 조건에 x,y 는 -1000 ~ 1000 이고,
+        # 좌표 2배를 했기 때문에 -2000 ~ 2000 이다.
+
+        # 배열을 사용할 수 있게 범위를 0 ~ 4000 으로 바꿔준다.
+        # 그러므로 OFFSET = 2000 으로 설정하고 더해준다.
+        x,y = x+OFFSET, y+OFFSET
+        
         marbles.append( (x,y,w,mapper[d],i) )
-    
+
+    # COORD_SIZE = 4000.
+    # 모든 구슬들은 (0,0) ~ (4000,4000) 사이에 있기 때문에
+    # 한방향으로 직전밖에 할수 없는 구슬들은
+    # simulate를 최대치인 4000번 반복하면, 모든 구슬들은 격자 밖으로 빠져나가게 된다.
+    # 즉 그 이후로는 simulate 할 필요가 없다.
     for i in range(1, COORD_SIZE +1):
+        # 한번 simulate 할때마다 1초씩 늘어난다.
         cur_time = i
         simulate()
-
+    
     print(last_collision_time)
