f25437345cf58ea1b3eb5b427114373db945cd28
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-19 23:35:50 +0900

    Title: 2명의 도둑, Time: 548ms, Memory: 29MB, Status: Passed - CodeTree

--- 240519/2명의 도둑/two-thieves.py
+++ 240519/2명의 도둑/two-thieves.py
@@ -1,61 +1,72 @@
 n, m, c = map(int, input().split())
-
 arr = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
-selected_list = []
+selected = []
+max_val = 0
 
-ans = 0
+def find_max_sum(cur_idx, cur_weight, cur_val):
+    global max_val
 
-def is_duplicated(x1, y1, x2, y2):
-    return x1 == x2 and not (y1 + m <= y2 or y2 + m <= y1)
-
-def recursive(cnt):
-    global ans
-
-    if cnt == 2:
-        ans = max(ans, check())
+    if cur_idx == m:
+        if cur_weight <= c:
+            max_val = max(max_val, cur_val)
         return
 
-    for x1 in range(n):
-        for y1 in range(n - m + 1):
-            selected_list.append((x1, y1))
-            recursive(cnt + 1)
-            selected_list.pop()
+    # selected[cur_idx] 선택하지 않을 시
+    find_max_sum(cur_idx +1, cur_weight, cur_val)
 
-def check():
-    x1, y1 = selected_list[0]
-    x2, y2 = selected_list[1]
+    # selected[cur_idx] 선택시
+    # 무게 : + selected[cur_idx]
+    # 가치 : + selected[cur_idx] ** 2
+    find_max_sum(cur_idx +1, cur_weight + selected[cur_idx], 
+                 cur_val + selected[cur_idx] ** 2)
 
-    if is_duplicated(x1, y1, x2, y2):
-        return 0
+# (x,y) ~ (x, y+ m-1) 까지 숫자들 중에서 고른다
+# 무게의 합이 c를 넘지 않으면서 얻을수 있는 최대 value를 반환
+def find_max(x, y):
+    global selected, max_val
 
-    cost1 = getCost(x1, y1)
-    cost2 = getCost(x2, y2)
+    selected = arr[x][y:y + m]
 
-    return cost1 + cost2
+    max_val = 0
+    find_max_sum(0,0,0)
+    return max_val
 
-def getCost(x, y):
-    weights = arr[x][y:y + m]
-    n = len(weights)
-    max_value = 0
+# 두 선분 [a,b], [c,d] 가 겹치는지 확인
+def intersect(a,b,c,d):
+    # 겹치지 않는 경우의 반대를 return.
+    return not (b < c or d < a)
 
-    def backtrack(index, current_weight, current_value):
-        nonlocal max_value
-        if current_weight > c:
-            return
-        max_value = max(max_value, current_value)
-        if index == n:
-            return
-        # 선택하지 않는 경우
-        backtrack(index + 1, current_weight, current_value)
-        # 선택하는 경우
-        backtrack(index + 1, current_weight + weights[index], current_value + weights[index] ** 2)
+# 두 도둑의 위치가 올바른지 확인
+def possible(x1,y1, x2,y2):
+    # 범위가 격자를 벗어나는 경우 : False
+    if y1 + m-1 >= n or y2 + m-1 >= n:
+        return False
 
-    backtrack(0, 0, 0)
-    return max_value
+    # 두 훔칠 위치의 행이 다르다
+    # 항상 겹치지 않다 : True
+    if x1 != x2:
+        return True
 
-recursive(0)
+    # 두 구간이 겹친다 : False
+    if intersect(y1, y1 + m-1, y2, y2 + m-1):
+        return False
+
+    # 그 외 (격자 안에 있고, 행은 같은데, 겹치지 않음)
+    return True
+
+# 첫번째 도둑 : (x1,y1) ~ (x1, y1 + m-1)
+# 두번째 도둑 : (x2,y2) ~ (x2, y2 + m-1)
+# 가능한 모든 위치 탐색
+ans = max([
+    find_max(x1,y1) + find_max(x2,y2)
+    for x1 in range(n)
+    for y1 in range(n)
+    for x2 in range(n)
+    for y2 in range(n)
+    if possible(x1,y1,x2,y2)
+])
 print(ans)
