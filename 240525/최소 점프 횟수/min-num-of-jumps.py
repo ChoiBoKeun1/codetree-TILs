b3626fad8c18710b0a7a6cfe9db6b4da16d543a4
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-05-25 13:29:30 +0900

    Title: 최소 점프 횟수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240525/최소 점프 횟수/min-num-of-jumps.py
+++ 240525/최소 점프 횟수/min-num-of-jumps.py
@@ -1,27 +1,24 @@
 import sys
-
 MIN = sys.maxsize
+min_val = MIN
 
 n = int(input())
 arr = list(map(int,input().split()))
-
-cur_idx = 0
-
-min_val = MIN
+selected = []
 
 def move(cnt):
-    global cur_idx, min_val
+    global min_val
 
-    if cur_idx == n-1:
-        min_val = min(min_val, cnt)
+    if cnt == n-1:
+        min_val = min(min_val, len(selected))
         return
 
-    dist = arr[cur_idx]
+    for i in range(1, arr[cnt]+1):
+        if cnt + i <= n-1:
+            selected.append(i)
+            move(cnt + i)
+            selected.pop()
 
-    for i in range(1,dist):
-        if cur_idx + i <= n-1:
-            cur_idx += i
-            move(cnt + 1)
 
 move(0)
 ans = min_val if min_val != MIN else -1
