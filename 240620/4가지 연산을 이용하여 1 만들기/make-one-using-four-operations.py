39db3134ee474f3975324394a73c62e415d4dfcc
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-06-20 22:10:13 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 612ms, Memory: 79MB, Status: Passed - CodeTree

--- 240620/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240620/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,50 +1,75 @@
 from collections import deque
-q = deque()
+import sys
+import enum
+
+OPERATOR_NUM = 4
+INT_MAX = sys.maxsize
+
+class OPERATOR(enum.Enum):
+    SUBTRACT = 0
+    ADD = 1
+    DIV2 = 2
+    DIV3 = 3
 
 n = int(input())
+ans = INT_MAX
 
-MAX = 1000000
+q = deque()
+visited = [False for _ in range(2*n)]
 
-visited = [False] * (MAX +1)
-ans = 0
+# step[i] : n에서 시작하여 i 지점에 도달하기 위한 최단거리
+step = [0 for _ in range(2*n)]
+
+# 숫자 num 에 op 연산을 할수 있는가?
+def possible(num, op):
+    if op == OPERATOR.SUBTRACT.value:
+        return True
+    elif op == OPERATOR.ADD.value:
+        return True
+    elif op == OPERATOR.DIV2.value:
+        return num % 2 == 0
+    else:
+        return num % 3 == 0
+
+# 숫자 num 에 op 연산 수행
+def calc(num, op):
+    if op == OPERATOR.SUBTRACT.value:
+        return num -1
+    elif op == OPERATOR.ADD.value:
+        return num +1
+    elif op == OPERATOR.DIV2.value:
+        return num // 2
+    else:
+        return num // 3
+
+def in_range(num):
+    return 1 <= num and num <= 2*n - 1
+
+def can_go(num):
+    return in_range(num) and not visited[num]
+
+def push(num, new_step):
+    q.append(num)
+    visited[num] = True
+    step[num] = new_step
 
 def bfs():
     global ans
 
-    q.append((n,0))
-    visited[n] = True
-
-    next = 0
     while q:
-        now,cnt = q.popleft()
+        cur_num = q.popleft()
 
-        if now == 1:
-            ans = cnt
-            return
+        for i in range(OPERATOR_NUM):
+            if not possible(cur_num, i):
+                continue
 
-        next = now +1
-        if next <= MAX and not visited[next]:
-            q.append((next, cnt +1))
-            visited[next] = True
+            new_num = calc(cur_num, i)
 
-        next = now -1
-        if next >= 1 and not visited[next]:
-            q.append((next, cnt +1))
-            visited[next] = True
+            if can_go(new_num):
+                push(new_num, step[cur_num] + 1)
         
-        if now % 2 == 0:
-            next = now // 2
-            if not visited[next]:
-                q.append((next, cnt +1))
-                visited[next] = True
+        ans = step[1]
 
-        if now % 3 == 0:
-            next = now // 3
-            if not visited[next]:
-                q.append((next, cnt +1))
-                visited[next] = True
-
-
-
+push(n,0)
 bfs()
 print(ans)
