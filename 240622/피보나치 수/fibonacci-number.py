8b1f83c1b808b26c343b94fcc65819d68f79cc32
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-06-22 10:47:09 +0900

    Title: 피보나치 수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240622/피보나치 수/fibonacci-number.py
+++ 240622/피보나치 수/fibonacci-number.py
@@ -1,17 +1,36 @@
 n = int(input())
 
-memo = [-1] * 46
+# using recursive
+'''def fibbo(n):
+    if n == 1 or n == 2:
+        return 1
+    
+    return fibbo(n-1) + fibbo(n-2)
 
+print(fibbo(n))
+'''
+# using memoization
+'''memo = [-1] * (n+1)
 def fibbo(n):
     if memo[n] != -1:
         return memo[n]
 
     if n <= 2:
         memo[n] = 1
-
     else:
         memo[n] = fibbo(n-1) + fibbo(n-2)
 
     return memo[n]
 
 print(fibbo(n))
+'''
+# using tabulation
+MAX_NUM = 45
+dp = [0 for _ in range(MAX_NUM + 1)]
+
+dp[1] = dp[2] = 1
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
