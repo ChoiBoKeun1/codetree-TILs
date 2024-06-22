bc9b9a22a7645f3d97e665d13cd50d0b3bcdb272
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-06-22 10:43:31 +0900

    Title: 피보나치 수, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240622/피보나치 수/fibonacci-number.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+memo = [-1] * 46
+
+def fibbo(n):
+    if memo[n] != -1:
+        return memo[n]
+
+    if n <= 2:
+        memo[n] = 1
+
+    else:
+        memo[n] = fibbo(n-1) + fibbo(n-2)
+
+    return memo[n]
+
+print(fibbo(n))
