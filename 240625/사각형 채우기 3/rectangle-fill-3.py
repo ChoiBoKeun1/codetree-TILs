d1f6b8b1555b665e02d78341cf379e29f42c0132
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-06-25 11:41:38 +0900

    Title: 사각형 채우기 3, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240625/사각형 채우기 3/rectangle-fill-3.py
+++ 240625/사각형 채우기 3/rectangle-fill-3.py
@@ -2,22 +2,17 @@ n = int(input())
 
 MOD = 1000000007
 
-dp = [0] * (1001)
-dphalf = [0] * (1001)
+dp = [0] * (n+1)
 
-# dp[i]. 2*i 사각형을 채우는 방법의 수
-
-# dp[0] = 1. 사각형을 배치하지 않는 경우
-
-# dp[1]. 2*1 사각형을 채우는 방법의 수
-# 1*1 2개 or 2*1 1개. dp[1] = 2
-
+dp[0] = 1
 dp[1] = 2
-dp[2] = 7
-dphalf[1] = 1
-dphalf[2] = 3
-for i in range(3, n+1):
-    dp[i] = (dp[i-1]*2 + dp[i-2] + dphalf[i-1] * 2 )  %1000000007
-    dphalf[i] = (dp[i-1] + dphalf[i-1]) %1000000007
+
+# dp[i] = dp[i-1] * 2 + dp[i-2] * 3 +
+#           (dp[i-3] + dp[i-4] + dp[i-5] + ... dp[0]) * 2
+
+for i in range(2, n+1):
+    dp[i] = (dp[i-1] * 2 + dp[i-2] * 3) % MOD
+    for j in range(i-3, -1, -1):
+        dp[i] = (dp[i] + dp[j] * 2) % MOD
 
 print(dp[n])
