94983358c8a27df2b124f15df9303e11602f9895
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-06-28 11:59:34 +0900

    Title: 정수 사각형 최대 합, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 240628/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240628/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -14,14 +14,8 @@ def initialize():
     for i in range(1, n):
         dp[i][0] = arr[i][0] + dp[i-1][0]
 
-    for i in range(1, n):
-        dp[n-1][i] = arr[n-1][i] + dp[n-1][i-1]
-
-    for i in range(1, n):
-        dp[0][i] = arr[0][i] + dp[0][i-1]
-
-    for i in range(1, n):
-        dp[i][n-1] = arr[i][n-1] + dp[i-1][n-1]
+    for j in range(1, n):
+        dp[0][j] = arr[0][j] + dp[0][j-1]
 
 initialize()
 
