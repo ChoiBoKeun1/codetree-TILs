d9bba6b2471d38d0682073dfffa2250a8f85258c
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-01 13:06:29 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 830ms, Memory: 54MB, Status: Passed - CodeTree

--- 240701/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240701/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -15,12 +15,17 @@ cells = list()
 ans = 0
 
 def initialize():
+    # arr에서 작은값부터 탐색을 진행하기 위해
+    # cells 에 (값, i, j) 를 넣고
+    # sort를 써서 작은값부터 나오도록 정렬
     for i in range(n):
         for j in range(n):
             cells.append((arr[i][j], i, j))
-
+    
+    # 오름차순 정렬
     cells.sort()
 
+    # dp 1로 초기화
     for i in range(n):
         for j in range(n):
             dp[i][j] = 1
@@ -33,6 +38,7 @@ for _, x, y in cells:
 
     for dx,dy in zip(dxs,dys):
         nx, ny = x + dx, y + dy
+        
         if in_range(nx,ny) and arr[nx][ny] > arr[x][y]:
             dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
 
