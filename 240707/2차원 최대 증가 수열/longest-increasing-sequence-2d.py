885eef805fe7050433c16697e1b752587a0ae01a
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-07 18:22:15 +0900

    Title: 2차원 최대 증가 수열, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240707/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240707/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,25 +1,35 @@
+import sys
+INT_MIN = -sys.maxsize
+
 n,m = map(int,input().split())
 arr = [
     list(map(int,input().split()))
     for _ in range(n)
 ]
-# -1 : 방문하지 않았음을 의미
 dp = [
-    [-1] * m
+    [INT_MIN] * m
     for _ in range(n)
 ]
+dp[0][0] = 1
 
-def dfs(x,y):
-    if dp[x][y] != -1:
-        return dp[x][y]
-    
-    dp[x][y] = 1
+# 점프할 지점 (i,j)
+for i in range(n):
+    for j in range(m):
+        # 점프 직전 위치 (x,y)
+        for x in range(i):
+            for y in range(j):
+                # 점프 직전 위치 (x,y) 값이 INT_MIN 이다
+                # -> (x,y) 에서 점프하는건 근본적으로 불가능하다
+                if dp[x][y] == INT_MIN:
+                    continue
+                
+                # 값이 증가하는 경우에만 갱신
+                if arr[x][y] < arr[i][j]:
+                    dp[i][j] = max(dp[i][j], dp[x][y] +1)
 
-    for i in range(x + 1, n):
-        for j in range(y + 1, m):
-            if arr[i][j] > arr[x][y]:  # 점프한 곳의 값이 더 큰 경우
-                dp[x][y] = max(dp[x][y], dfs(i, j) +1)
-    
-    return dp[x][y]
-
-print(dfs(0,0))
+# 최댓값
+ans = INT_MIN
+for i in range(n):
+    for j in range(m):
+        ans = max(ans, dp[i][j])
+print(ans)
