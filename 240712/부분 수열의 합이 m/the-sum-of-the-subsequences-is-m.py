b5c8131c06c9a20bd5170a157ec72b244b9bd279
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-12 12:00:18 +0900

    Title: 부분 수열의 합이 m, Time: 142ms, Memory: 73MB, Status: Passed - CodeTree

--- 240712/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
+++ 240712/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -1,8 +1,10 @@
 n,m = map(int,input().split())
 nums = list(map(int,input().split()))
 
+NUM_MAX = 10001
+
 # dp[i] : 지금까지 선택한 숫자의 합이 i 일때, 가능한 최소 숫자 개수
-dp = [10001] * (m+1)
+dp = [NUM_MAX] * (m+1)
 dp[0] = 0
 
 for i in range(n):
@@ -10,4 +12,5 @@ for i in range(n):
         if j >= nums[i]:
             dp[j] = min(dp[j], dp[j - nums[i]] + 1)
 
-print(dp[m])
+ans = -1 if dp[m] == NUM_MAX else dp[m]
+print(ans)
