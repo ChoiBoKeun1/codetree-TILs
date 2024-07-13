425b66f572fe922cd6609252e3b2407995f04ebc
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-13 11:09:30 +0900

    Title: 1, 2, 5 더하기, Time: 197ms, Memory: 25MB, Status: Passed - CodeTree

--- 240713/1, 2, 5 더하기/1-2-5-plus.py
+++ 240713/1, 2, 5 더하기/1-2-5-plus.py
@@ -1,23 +1,21 @@
-n = int(input())
-
+MAX_M = 3
 MOD = 10007
 
-def count_ways_recursive(n, memo=None):
-    if memo is None:
-        memo = {}
-        
-    # 기본 케이스
-    if n == 0:
-        return 1
-    if n < 0:
-        return 0
-    
-    # 메모이제이션 확인
-    if n in memo:
-        return memo[n]
-    
-    # 1, 2, 5를 사용하여 재귀적으로 방법의 수를 계산
-    memo[n] = count_ways_recursive(n - 1, memo) + count_ways_recursive(n - 2, memo) + count_ways_recursive(n - 5, memo)
-    return memo[n]
+n = int(input())
 
-print(count_ways_recursive(n) % 10007)
+# dp[i] : i를 1,2,5 의 합으로 나타내는 방법의 수
+dp = [0] * (n+1)
+numbers = [1,2,5]
+
+# 0을 만드는 방법 : 아무것도 더하지 않는 방법 1가지
+dp[0] = 1
+
+# dp 갱신
+# 외부 for문 i : 현재 목표 합계
+# 내부 for문 : 1,2,5 세 숫자 순회
+for i in range(1, n+1):
+    for j in range(MAX_M):
+        if i >= numbers[j]:
+            dp[i] = (dp[i] + dp[i-numbers[j]]) % MOD
+
+print(dp[n])
