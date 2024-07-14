b6fdf61dbf0606e95c6bcf889441e27b146dce9d
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-14 20:20:11 +0900

    Title: 최대 동전 거슬러주기, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240714/최대 동전 거슬러주기/max-coin-change.py
+++ 240714/최대 동전 거슬러주기/max-coin-change.py
@@ -1,13 +1,15 @@
 n,m = map(int,input().split())
 coins = list(map(int,input().split()))
 
-# dp[i] : 합이 i 일때, 가능한 경우의 수
-dp = [0] * (m+1)
+MAX = 10001
+
+# dp[i] : 합이 i 일때, 가능한 최대 동전의 개수
+dp = [-1] * (m+1)
+dp[0] = 0
 
 for i in range(1, m+1):
-    for j in range(n):
-        if i >= coins[j]:
-            dp[i] = max(dp[i], dp[i - coins[j]] + 1)
+    for coin in coins:
+        if i >= coin and dp[i - coin] != -1:
+            dp[i] = max(dp[i], dp[i - coin] + 1)
 
-ans = -1 if dp[m] == 0 else dp[m]
-print(ans)
+print(dp[m])
