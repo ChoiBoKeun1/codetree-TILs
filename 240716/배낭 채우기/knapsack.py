17a44d9b06ddd9fa346b05696ba380f6156b5442
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-16 10:23:06 +0900

    Title: 배낭 채우기, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/배낭 채우기/knapsack.py
+++ 240716/배낭 채우기/knapsack.py
@@ -1,22 +1,16 @@
 n,m = map(int,input().split())
-jewels = []
-
-
+items = []
 for _ in range(n):
-    w,v = map(int,input().split())
-    jewels.append((w,v))
+    items.append(tuple(map(int,input().split())))
+    
+# dp[i] : 무게 i 일때, 최대 value
+dp = [0] * (m+1)
 
-jewels.sort()
+# 각 보석에 대해
+for weight, value in items:
+    # 현재 보석을 선택하는 경우
+    # dp를 뒤에서부터 갱신
+    for w in range(m, weight -1, -1):
+        dp[w] = max(dp[w], dp[w - weight] + value)
 
-weight, value = 0,0
-
-for jewel in jewels:
-    w,v = jewel
-
-    weight += w
-    if weight > m:
-        weight -= w
-        continue
-    value += v
-
-print(value)
+print(dp[m])
