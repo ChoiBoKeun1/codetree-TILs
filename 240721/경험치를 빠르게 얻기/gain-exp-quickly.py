99ff71716235a8ffdfdf162fe8b8f84a1b2813d4
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-21 15:51:12 +0900

    Title: 경험치를 빠르게 얻기, Time: 151ms, Memory: 28MB, Status: Passed - CodeTree

--- 240721/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 240721/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -18,39 +18,35 @@ t = sum(runtime)
 # 얻을수 있는 최대 경험치.
 dp = [
     [-sys.maxsize] * (t + 1) 
-    for _ in range(2)
+    for _ in range(n+1)
 ]
-# 퀘스트를 하나도 진행하지 않았고 총 시간이 0 이면
+# 0번째 퀘스트 까지 고려했을때 총 시간은 0 이다.
 # 얻을수 있는 경험치도 0 이다.
 dp[0][0] = 0
 
 # dp 배열 갱신
 for i in range(1, n + 1):
-    a = i % 2  # 현재 채울 index
-    b = 1 - a  # 가져올 index (a가 0이면 1, a가 1이면 0이 되도록 설정)
-
-    # 현재 채울 index는 재사용을 위해 값을 초기화.
     for j in range(t + 1):
-        dp[a][j] = -sys.maxsize
-
-    for j in range(t + 1):
-        # Case 1. 현재 퀘스트를 진행하여 수행시간의 총 합이 j가 되기 위해서는
+        # Case 1. 현재 퀘스트를 진행하여 
+        #         수행시간의 총 합이 j가 되기 위해서는
         #         i - 1번째 퀘스트 까지 수행시간이 j - runtime[i]가 되어야 한다.
         if j - runtime[i] >= 0:
-            dp[a][j] = max(dp[a][j], dp[b][j - runtime[i]] + exp[i])
+            dp[i][j] = max(dp[i][j], dp[i-1][j - runtime[i]] + exp[i])
 
-        # Case 2. 현재 퀘스트를 진행하지 않고 수행시간의 총 합이 j가 되기 위해서는
+        # Case 2. 현재 퀘스트를 진행하지 않고 
+        #         수행시간의 총 합이 j가 되기 위해서는
         #         i - 1번째 퀘스트 까지 수행시간이 j가 되어야 한다.
-        dp[a][j] = max(dp[a][j], dp[b][j])
+        dp[i][j] = max(dp[i][j], dp[i-1][j])
 
-# n개의 퀘스트까지 고려했을 때 최대 경험치 합이 m 이상인 경우 중 
+# n개의 퀘스트까지 고려했을 때 
+# 최대 경험치 합이 m 이상인 경우 중 
 # 최소 시간을 계산.
 ans = sys.maxsize
 for j in range(t + 1):
-    if dp[n % 2][j] >= m:
+    if dp[n][j] >= m:
         ans = min(ans, j)
 
-# 불가능하다면 -1이 답이 됩니다.
+# 불가능하다면 -1
 if ans == sys.maxsize:
     ans = -1
 
