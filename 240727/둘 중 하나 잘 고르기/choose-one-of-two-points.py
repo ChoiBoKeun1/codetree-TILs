76d58305dd43c5c4da5904d3aec1c5d55decc1b3
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-27 12:30:37 +0900

    Title: 둘 중 하나 잘 고르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240727/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -0,0 +1,25 @@
+n = int(input())
+cards = [
+    tuple(map(int, input().split())) 
+    for _ in range(2*n)
+]
+
+# DP 테이블 초기화
+dp = [
+    [-1] * (n+1) 
+    for _ in range(n+1)
+]
+dp[0][0] = 0
+
+# DP 테이블 갱신
+for k in range(2*n):
+    red, blue = cards[k]
+    for i in range(min(k + 1, n), -1, -1):
+        for j in range(min(k + 1, n), -1, -1):
+            if i > 0:
+                dp[i][j] = max(dp[i][j], dp[i-1][j] + red)
+            if j > 0:
+                dp[i][j] = max(dp[i][j], dp[i][j-1] + blue)
+
+# 결과 출력
+print(dp[N][N])
