8ae0fa516a6561e5016de79cfcba0d363109c705
ChoiBoKeun1 <136104922+ChoiBoKeun1@users.noreply.github.com>
2024-07-27 12:42:05 +0900

    Title: 둘 중 하나 잘 고르기, Time: 238ms, Memory: 26MB, Status: Passed - CodeTree

--- 240727/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 240727/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -11,13 +11,22 @@ dp = [
 ]
 dp[0][0] = 0
 
-# DP 테이블 갱신
+# DP 갱신
+# 현재 카드 : cards[k]
 for k in range(2*n):
     red, blue = cards[k]
-    for i in range(min(k + 1, n), -1, -1):
-        for j in range(min(k + 1, n), -1, -1):
+
+    # 역순으로 순회 n ~ 0
+    # i : 선택한 빨간 카드의 개수
+    # j : 선택한 파란 카드의 개수
+    for i in range(n, -1, -1):
+        for j in range(n, -1, -1):
+            # 빨간 카드를 선택한 경우
+            # 이전거에서 + red
             if i > 0:
                 dp[i][j] = max(dp[i][j], dp[i-1][j] + red)
+            # 파란 카드를 선택한 경우
+            # 이전거에서 + blue
             if j > 0:
                 dp[i][j] = max(dp[i][j], dp[i][j-1] + blue)
 
